6a3dc25d922d23c3929edb8f157596963857e914
krasilnikov <dkrasilnikov@gmail.com>
2016-07-09 01:17:18 +1000

    под поддержку пространств имен перепилены основные интерфейсы. Поддержка пространств имен реализована в мета-репозитории и дата-репозитории.

--- core/impl/dbSync/ionDbSync.js
+++ core/impl/dbSync/ionDbSync.js
@@ -5,57 +5,56 @@
 
 var DbSync = require('core/interfaces/DbSync');
 var debug = require('debug-log')('ION:dbSync');
-var mongo = require('mongodb');
 
-function IonDbSync(connection,config){
+function IonDbSync(connection, config) {
 
-  var me = this;
-
-  /**
-   * @type {string}
-   */
-  this.metaTableName = "ion_meta";
+  var _this = this;
 
   /**
-   * @type {string}
+   * @type {String}
    */
-  this.viewTableName = "ion_view";
+  this.metaTableName = 'ion_meta';
 
   /**
-   * @type {string}
+   * @type {String}
    */
-  this.navTableName = "ion_nav";
+  this.viewTableName = 'ion_view';
+
+  /**
+   * @type {String}
+   */
+  this.navTableName = 'ion_nav';
 
   /**
    * @type {Db}
    */
   this.db = connection;
 
-  if(config.metadata){
-    if(config.metadata.MetaTableName){
-      this.metaTableName = config.metadata.MetaTableName;
+  if (config.metaTables) {
+    if (config.metaTables.MetaTableName) {
+      this.metaTableName = config.metaTables.MetaTableName;
     }
-    if(config.metadata.ViewTableName){
-      this.viewTableName = config.metadata.ViewTableName;
+    if (config.metaTables.ViewTableName) {
+      this.viewTableName = config.metaTables.ViewTableName;
     }
-    if(config.metadata.NavTableName){
-      this.navTableName = config.metadata.NavTableName;
+    if (config.metaTables.NavTableName) {
+      this.navTableName = config.metaTables.NavTableName;
     }
   }
 
   /**
-   * @param {object} cm
+   * @param {{}} cm
    * @returns {Promise}
    * @private
    */
-  this._createCollection = function(cm){
-    return new Promise(function(resolve, reject){
-      var collection = me.db.collection(cm.name,function(err, collection){
-        if (err){
+  this._createCollection = function (cm) {
+    return new Promise(function (resolve, reject) {
+      var collection = _this.db.collection(cm.name, function (err, collection) {
+        if (err) {
           return reject(err);
         }
         if (!collection) {
-          me.db.createCollection(cm.name).then(resolve).catch(reject);
+          _this.db.createCollection(cm.name).then(resolve).catch(reject);
         } else {
           resolve(collection);
         }
@@ -64,35 +63,37 @@ function IonDbSync(connection,config){
   };
 
   /**
-   * @param {object} cm
+   * @param {{}} cm
    * @private
    */
-  this._addIndexes = function(cm){
+  this._addIndexes = function (cm) {
     /**
      * @param {Collection} collection
      */
-    return function(collection){
-      var i, promises = [];
+    return function (collection) {
+      var i, promises;
+      promises = [];
 
-      function createIndexPromise(props, unique){
+      function createIndexPromise(props, unique) {
         return new Promise(
-          function(resolve, reject) {
-            var opts = {}, i;
-            if (unique){
+          function (resolve, reject) {
+            var opts, i;
+            opts = {};
+            if (unique) {
               opts.unique = true;
             }
 
             var indexDef = {};
-            if (typeof props === "string") {
+            if (typeof props === 'string') {
               indexDef = props;
             } else if (Array.isArray(props)) {
               for (i = 0; i < props.length; i++) {
                 indexDef[props[i]] = 1;
               }
             }
-            collection.createIndex(indexDef, opts, function(err, iname){
+            collection.createIndex(indexDef, opts, function (err, iname) {
               /*
-              if (err) {
+              If (err) {
                 return reject(err);
               }
               */
@@ -102,25 +103,25 @@ function IonDbSync(connection,config){
         );
       }
 
-      return new Promise(function(resolve, reject){
+      return new Promise(function (resolve, reject) {
         var promises = [];
         promises.push(createIndexPromise(cm.key, true));
-        promises.push(createIndexPromise("_class", false));
+        promises.push(createIndexPromise('_class', false));
 
         for (i = 0; i < cm.properties.length; i++) {
-          if (cm.properties[i].type === 13 || (cm.properties[i].indexed === true)){
+          if (cm.properties[i].type === 13 || (cm.properties[i].indexed === true)) {
             promises.push(createIndexPromise(cm.properties[i].name, cm.properties[i].unique));
           }
         }
 
         if (cm.compositeIndexes) {
-          for(i = 0; i < cm.compositeIndexes.length; i++) {
+          for (i = 0; i < cm.compositeIndexes.length; i++) {
             promises.push(createIndexPromise(cm.compositeIndexes[i].properties, cm.compositeIndexes[i].unique));
           }
         }
 
         Promise.all(promises).
-          then(function(inames){
+          then(function (inames) {
             resolve(collection);
           }).
           catch(reject);
@@ -129,45 +130,45 @@ function IonDbSync(connection,config){
   };
 
   /**
-   * @param {object} cm
+   * @param {{}} cm
    * @returns {Promise}
    */
-  function findClassRoot(cm){
-    if (!cm.ancestor){
-      return new Promise(function(resolve, reject){
+  function findClassRoot(cm) {
+    if (!cm.ancestor) {
+      return new Promise(function (resolve, reject) {
         resolve(cm);
       });
     }
-    return new Promise(function(resolve, reject) {
-      me.db.collection(me.metaTableName, function (err, collection) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.metaTableName, function (err, collection) {
         if (err) {
           return reject(err);
         }
-        collection.findOne({name:cm.ancestor}, function(err, anc){
+        collection.findOne({name: cm.ancestor}, function (err, anc) {
           if (err) {
             return reject(err);
           }
           if (anc) {
             return findClassRoot(anc).then(resolve).catch(reject);
           }
-          reject({Error:"Класс " + cm.ancestor + " не найден!"});
+          reject({Error: 'Класс ' + cm.ancestor + ' не найден!'});
         });
       });
     });
   }
 
   /**
-   * @param {object} classMeta
+   * @param {{}} classMeta
    * @returns {Promise}
    * @private
    */
-  this._defineClass = function(classMeta){
-    return new Promise(function(resolve, reject) {
-      findClassRoot(classMeta).then(function(cm){
-        me._createCollection(cm).
+  this._defineClass = function (classMeta, namespace) {
+    return new Promise(function (resolve, reject) {
+      findClassRoot(classMeta).then(function (cm) {
+        _this._createCollection(cm).
           then(me._addIndexes(classMeta)).
-          then(function() {
-            me.db.collection(me.metaTableName, function (err, collection) {
+          then(function () {
+            _this.db.collection(me.metaTableName, function (err, collection) {
               if (err) {
                 return reject(err);
               }
@@ -186,9 +187,9 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._undefineClass = function(className, version){
-    return new Promise(function(resolve, reject) {
-      me.db.collection(me.metaTableName, function(err, collection) {
+  this._undefineClass = function (className, version, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.metaTableName, function (err, collection) {
         var query = {name: className};
         if (version) {
           query.version = version;
@@ -203,21 +204,21 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._defineView = function(viewMeta, className, type, path){
-    return new Promise(function(resolve, reject) {
-        viewMeta.type = type;
-        viewMeta.className = className;
-        if (path !== null) {
-          viewMeta.path = path;
-        } else {
-          reject(new Error('не передан path'));
-        }
-        me.db.collection(me.viewTableName, function(err, viewCollection){
+  this._defineView = function (viewMeta, className, type, path, namespace) {
+    return new Promise(function (resolve, reject) {
+      viewMeta.type = type;
+      viewMeta.className = className;
+      if (path !== null) {
+        viewMeta.path = path;
+      } else {
+        reject(new Error('не передан path'));
+      }
+      _this.db.collection(me.viewTableName, function (err, viewCollection) {
           if (err) {
             return reject(err);
           }
-          viewCollection.insertOne(viewMeta, function(err, vm){
-            if(err){
+          viewCollection.insertOne(viewMeta, function (err, vm) {
+            if (err) {
               return reject(err);
             }
             resolve(vm);
@@ -226,10 +227,10 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._undefineView = function(className, type, path, version){
-    return new Promise(function(resolve, reject) {
-        me.db.collection(me.viewTableName, function(err, viewCollection){
-          if(err){
+  this._undefineView = function (className, type, path, version, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.viewTableName, function (err, viewCollection) {
+          if (err) {
             reject(err);
           }
 
@@ -237,8 +238,8 @@ function IonDbSync(connection,config){
           if (version) {
             query.version = version;
           }
-          viewCollection.remove(query,function(err,vm){
-            if(err){
+          viewCollection.remove(query, function (err,vm) {
+            if (err) {
               reject(err);
             }
             resolve(vm);
@@ -247,20 +248,20 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._defineNavSection = function(navSection){
-    return new Promise(function(resolve, reject) {
-        me.db.collection(me.navTableName,
-          /**
-           * @param err
-           * @param {Collection} navCollection
-           */
-          function(err, navCollection){
+  this._defineNavSection = function (navSection, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.navTableName,
+        /**
+         * @param err
+         * @param {Collection} navCollection
+         */
+         function (err, navCollection) {
             navSection.itemType = 'section';
-            if(!navSection.nodes){
+            if (!navSection.nodes) {
               navSection.nodes = [];
             }
-            navCollection.updateOne({name:navSection.name}, navSection, {upsert: true}, function(err, ns){
-              if(err){
+            navCollection.updateOne({name: navSection.name}, navSection, {upsert: true}, function (err, ns) {
+              if (err) {
                 reject(err);
               }
               resolve(ns);
@@ -270,15 +271,15 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._undefineNavSection = function(sectionName){
-    return new Promise(function(resolve, reject) {
-      me.db.collection(me.navTableName, function(err, navCollection){
-        if(err){
+  this._undefineNavSection = function (sectionName, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.navTableName, function (err, navCollection) {
+        if (err) {
           reject(err);
         }
-        var query = {name:sectionName};
-        navCollection.remove(query,function(err,nsm){
-          if(err){
+        var query = {name: sectionName};
+        navCollection.remove(query, function (err,nsm) {
+          if (err) {
             reject(err);
           }
           resolve(nsm);
@@ -287,16 +288,16 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._defineNavNode = function(navNode,navSectionName){
-    return new Promise(function(resolve, reject) {
-      me.db.collection(me.navTableName, function(err, navCollection){
-        if(err){
+  this._defineNavNode = function (navNode,navSectionName, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.navTableName, function (err, navCollection) {
+        if (err) {
           reject(err);
         }
         navNode.itemType = 'node';
         navNode.section = navSectionName;
-        navCollection.updateOne({code:navNode.code}, navNode,{upsert: true},function(err, ns){
-          if(err){
+        navCollection.updateOne({code: navNode.code}, navNode, {upsert: true}, function (err, ns) {
+          if (err) {
             reject(err);
           }
           resolve(ns);
@@ -305,12 +306,12 @@ function IonDbSync(connection,config){
     });
   };
 
-  this._undefineNavNode = function(navNodeName){
-    return new Promise(function(resolve, reject) {
-      me.db.collection(me.navTableName, function(err, navCollection){
-        var query = {code:navNodeName};
-        navCollection.remove(query,function(err,nnm){
-          if(err){
+  this._undefineNavNode = function (navNodeName, namespace) {
+    return new Promise(function (resolve, reject) {
+      _this.db.collection(me.navTableName, function (err, navCollection) {
+        var query = {code: navNodeName};
+        navCollection.remove(query, function (err,nnm) {
+          if (err) {
             reject(err);
           }
           resolve(nnm);
