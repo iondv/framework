d7f784dd0b220e65287e6311c20fa89a36686377
krasilnikov <dkrasilnikov@gmail.com>
2016-07-18 18:37:04 +1000

    #IONCORE-70 Запил модуля di

---
+++ core/di.js
@@ -0,0 +1,29 @@
+/**
+ * Created by kras on 18.07.16.
+ */
+'use strict';
+
+/**
+ * @param {{name: String, module: String, config: {}, initMethod: String, initLevel: Number}} component
+ */
+function loadComponent(component, reg) {
+  var constructor
+}
+
+
+/**
+ * @param {Array} components
+ * @returns {Promise}
+ */
+function di(components) {
+  var reg = {};
+  var
+  for (var i = 0; i < components.length; i++) {
+
+  }
+
+  return new Promise(function (resolve, reject) {
+  });
+}
+
+module.exports = di;
