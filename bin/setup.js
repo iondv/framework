9ae9980c55b74d9b49e01f3625edc979bb068d7e
krasilnikov <dkrasilnikov@gmail.com>
2016-08-22 23:15:29 +1000

    #IONCORE-96 изменен сиснтаксис вызова

--- bin/setup.js
+++ bin/setup.js
@@ -4,48 +4,38 @@
 var worker = require('../lib/deploy');
 var config = require('../config');
 var di = require('core/di');
+var path = require('path');
 
 var IonLogger = require('core/impl/log/IonLogger');
 
 var sysLog = new IonLogger({});
 
-var setSrc = false;
+if (process.argv.length > 2) {
+  var app = process.argv[2];
+  var scope = null;
 
-var src = '';
-
-process.argv.forEach(function (val) {
-  if (val === '--src') {
-    setSrc = true;
-    return;
-  } else if (setSrc) {
-    src = val;
-  }
-  setSrc = false;
-});
-
-var scope = null;
-// Связываем приложение
-di('app', config.di,
-  {
-    sysLog: sysLog
-  },
-  null,
-  ['auth', 'rtEvents', 'sessionHandler']
-).then(
-  // Импорт
-  function (scp) {
-    scope = scp;
-    return worker(src);
-  }
-).then(function () {
-  return scope.dataSources.disconnect();
-}).then(
-  // Справились
-  function () {
-    console.info('Настройка выполнена успешно.');
-    process.exit(0);
-  }
-).catch(function (err) {
-  console.error(err);
-  process.exit(130);
-});
+  di('app', config.di,
+    {
+      sysLog: sysLog
+    },
+    null,
+    ['auth', 'rtEvents', 'sessionHandler']
+  ).then(
+    // Импорт
+    function (scp) {
+      scope = scp;
+      return worker(path.join(__dirname, '..', 'applications', app));
+    }
+  ).then(function () {
+    return scope.dataSources.disconnect();
+  }).then(
+    // Справились
+    function () {
+      console.info('Настройка выполнена успешно.');
+      process.exit(0);
+    }
+  ).catch(function (err) {
+    console.error(err);
+    process.exit(130);
+  });
+}
