d9b733c64635d92efbc3480760ea5ec631004517
krasilnikov <dkrasilnikov@gmail.com>
2016-08-29 13:29:50 +1000

    #IONSTUDIO-81 #MODREGISTR-62 фиксы2

--- core/impl/meta/DsMetaRepository.js
+++ core/impl/meta/DsMetaRepository.js
@@ -357,9 +357,12 @@ function DsMetaRepository(options) {
     var pm, i, j;
     for (i = 0; i < cm.plain.properties.length; i++) {
       if (cm.plain.properties[i].type === PropertyTypes.STRUCT) {
-        var structClass = getFromMeta(cm.plain.properties[i].refСlass, cm.plain.version, cm.getNamespace());
-        if (!structClass) {
-          throw new Error('Не найден класс структуры ' + cm.caption + '.' + cm.plain.properties[i].caption);
+        var structClass;
+        try {
+          structClass = getFromMeta(cm.plain.properties[i].refClass, cm.plain.version, cm.getNamespace());
+        } catch (err) {
+          throw new Error('Не найден класс [' + cm.plain.properties[i].refClass +
+            '] для структуры [' + cm.plain.caption + '].[' + cm.plain.properties[i].caption + ']');
         }
         if (!structClass.___structs_expanded) {
           expandStructs(structClass);
