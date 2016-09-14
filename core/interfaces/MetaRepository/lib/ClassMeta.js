316300a785543761a41af3a3000e680a00d91c94
krasilnikov <dkrasilnikov@gmail.com>
2016-09-14 21:16:59 +1000

    #IONCORE-115 Исправлен парсинг семантики

--- core/interfaces/MetaRepository/lib/ClassMeta.js
+++ core/interfaces/MetaRepository/lib/ClassMeta.js
@@ -60,11 +60,12 @@ function parseSemantics(semantics) {
 
   var getter = 'if(v&&v.trim()){var p=item.property(v);if(p) {return p.getDisplayValue();}}return v;';
 
-  var body = semantics.
-  replace(/([^\[\s\|]+)\s*\[\s*(\d+)\s*,\s*(\d+)\s*\]/g, 'getter(this,\'$1\').substr($2, $3)').
-  replace(/([^\[\s\|]+)\s*\[\s*(\d+)\s*\]/g, 'getter(this,\'$1\').substr($2)').
-  replace(/([^\s\|]+)\s*/g, 'getter(this,\'$1\')').
+  var body = semantics.replace(/\'/g, '\\\'').
+  replace(/([^\[\|]+)\s*\[\s*(\d+)\s*,\s*(\d+)\s*\]/g, 'getter(this,\'$1\').substr($2, $3)').
+  replace(/([^\[\|]+)\s*\[\s*(\d+)\s*\]/g, 'getter(this,\'$1\').substr($2)').
+  replace(/([^\|]+)\s*/g, 'getter(this,\'$1\')').
   replace(/\|/g, ' + ');
+  console.log(body);
   return new Function('', 'function getter(item, v) {' + getter + '} return ' + body);
 }
 
