758b7cb321d1ea33a1f8543c37f9787e28b638e0
Андрей К <akumidv@yandex.ru>
2016-09-22 19:29:56 +1000

    IONCORE-121 базовые файлы конвертора, формат описания трансформации между версиями

---
+++ bin/meta-update.js
@@ -0,0 +1,8 @@
+/**
+ * Created by akumidv on 21.09.2016.
+ */
+
+const metaVersion = require('lib/meta-update/meta-version');
+var TransfMD = require('lib/meta-update/transformation');
+
+TransfMD.verR2F0P0({});
