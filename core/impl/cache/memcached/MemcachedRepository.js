f4c63caaccbad01f9784a7e27963519fcee55901
Inkz <inkz@localhost.localdomain>
2016-10-03 23:22:30 +1000

    добавил интерфейс для модуля кэширования

---
+++ core/impl/cache/memcached/MemcachedRepository.js
@@ -0,0 +1,5 @@
+/**
+ * Created by inkz on 10/3/16.
+ */
+'use strict';
+
---
+++ core/interfaces/CacheRepository/CacheRepository.js
@@ -0,0 +1,41 @@
+/**
+ * Created by inkz on 10/3/16.
+ */
+'use strict';
+
+/**
+ *
+ * @constructor
+ */
+function CacheRepository() {
+  
+  /**
+   * 
+   * @returns {*}
+   */
+  this.init = function() {
+    return this._init();
+  };
+
+  /**
+   *
+   * @param key
+   * @returns {*}
+   */
+  this.get = function(key) {
+    return this._get(key);
+  };
+
+  /**
+   *
+   * @param key
+   * @param value
+   * @returns {*}
+   */
+  this.set = function(key, value) {
+    return this._set(key,value);
+  };
+
+}
+
+module.exports = CacheRepository;
