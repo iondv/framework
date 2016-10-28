2345bc6bd8a661a159d44401cb37c5c9a0cf8b34
krasilnikov <dkrasilnikov@gmail.com>
2016-10-28 17:12:43 +1000

    #IONCORE-135 #MODREGISTR-170 #MODREGISTR-77 Багфиксы сериализации мета-классов в JSON

--- core/interfaces/DataRepository/lib/Item.js
+++ core/interfaces/DataRepository/lib/Item.js
@@ -128,37 +128,49 @@ function Item(id, base, classMeta) {
   };
 
   /**
+   * @param {String} nm
+   * @param {ClassMeta} cm
+   */
+  function findPropertyMeta(nm, cm) {
+    var dot, pm;
+    if ((dot = nm.lastIndexOf('.')) > -1) {
+      pm = cm.getPropertyMeta(nm.substring(0, dot));
+      if (pm.type === PropertyTypes.REFERENCE) {
+        return findPropertyMeta(nm.substring(dot + 1), pm._refClass);
+      } else {
+        return null;
+      }
+    }
+    return cm.getPropertyMeta(nm);
+  }
+
+  /**
    * @param {String} name
    * @returns {Property | null}
    */
   this.property = function (name) {
-    var dot;
+    var pm;
     var props = this.getProperties();
     if (props.hasOwnProperty(name)) {
       return props[name];
-    } else if ((dot = name.lastIndexOf('.')) > -1) {
-      var cont = name.substring(0, dot);
-      var prop = this.property(cont);
-      if (prop.getType() === PropertyTypes.REFERENCE) {
-        initClassProps(prop.meta._refClass, cont);
-        if (this.properties.hasOwnProperty(name)) {
-          return this.properties[name];
-        }
+    } else {
+      pm = findPropertyMeta(name, this.classMeta);
+      if (pm) {
+        return new Property(this, pm);
       }
     }
     return null;
   };
 
-  function initClassProps(cm, container) {
+  function initClassProps(cm) {
     var pm = cm.getPropertyMetas();
     for (var i = 0; i < pm.length; i++) {
       if (pm[i].type !== PropertyTypes.STRUCT) {
-        var name = (container?container+'.':'') + pm[i].name;
-        _this.properties[name] = new Property(_this, pm[i]);
+        _this.properties[pm[i].name] = new Property(_this, pm[i]);
       }
     }
     if (cm.getAncestor()) {
-      initClassProps(cm.getAncestor(), container);
+      initClassProps(cm.getAncestor());
     }
   }
 
@@ -175,7 +187,7 @@ function Item(id, base, classMeta) {
 }
 
 Item.prototype.toString = function (semanticGetter, dateCallback) {
-  if(typeof semanticGetter === 'function'){
+  if (typeof semanticGetter === 'function') {
     return semanticGetter.call(this, dateCallback);
   }
   return this.classMeta.getSemantics(this, dateCallback);
