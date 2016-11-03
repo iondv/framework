8546fc74a0dfe3453255db91f7a11243c2a54f6e
krasilnikov <dkrasilnikov@gmail.com>
2016-11-04 00:07:10 +1000

    #IONCORE-48 реанимирован ConditionParser

--- core/ConditionParser.js
+++ core/ConditionParser.js
@@ -1,34 +1,12 @@
 /**
- * Created by kras on 31.08.16.
+ * Created by Данил on 10.10.2016.
  */
 'use strict';
 
 var ConditionTypes = require('core/ConditionTypes');
 var OperationTypes = require('core/OperationTypes');
-var equal = require('core/equal');
-var cast = require('core/cast');
 
-// jshint maxcomplexity: 40, eqeqeq: false
-function contains(container, content) {
-  if (container && content !== null) {
-    if (typeof container === 'string') {
-      if (content) {
-        return container.indexOf(String(content)) !== -1 ? true : false;
-      }
-    } else if (Array.isArray(container)) {
-      return container.indexOf(content) !== -1 ? true : false;
-    }
-  }
-  return false;
-}
-
-function toArray(v) {
-  if (!Array.isArray(v)) {
-    return [v];
-  }
-  return v;
-}
-
+// jshint maxcomplexity: 20
 function toScalar(v) {
   if (Array.isArray(v) && v.length) {
     return v.length ? v[0] : null;
@@ -37,82 +15,44 @@ function toScalar(v) {
 }
 
 /**
- * @param {Item} item
  * @param {{}} condition
- * @returns {Boolean}
+ * @returns {{}}
  */
-function checkCondition(item, condition) {
-  var pn, p, v, pv;
-  if (condition.property) {
-    pn = condition.property;
-    p = item.property(pn);
-    if (!p) {
-      throw new Error('Не найден указанный в условии атрибут ' + item.getClassName() + '.' + pn);
-    }
-    v = cast(condition.value, p.getType());
-    pv = item.property(pn).getValue();
+function ConditionParser(condition) {
+  var result = {};
+  if (condition.property !== null) {
     switch (condition.operation) {
-      case ConditionTypes.EQUAL:
-        return equal(pv, toScalar(v));
-      case ConditionTypes.NOT_EQUAL:
-        return !equal(pv, toScalar(v));
-      case ConditionTypes.EMPTY:
-        return !pv;
-      case ConditionTypes.NOT_EMPTY:
-        return pv ? true : false;
-      case ConditionTypes.LIKE:
-        return String(pv).match(new RegExp(toScalar(v))) ? true : false;
-      case ConditionTypes.LESS:
-        return pv < toScalar(v) ? true : false;
-      case ConditionTypes.MORE:
-        return pv > toScalar(v) ? true : false;
-      case ConditionTypes.LESS_OR_EQUAL:
-        return pv <= toScalar(v) ? true : false;
-      case ConditionTypes.MORE_OR_EQUAL:
-        return pv >= toScalar(v) ? true : false;
-      case ConditionTypes.IN:
-        return contains(toArray(v), pv);
-      case ConditionTypes.CONTAINS:
-        return contains(pv, toScalar(v));
+      case ConditionTypes.EQUAL: result[condition.property] = {$eq: toScalar(condition.value)}; break;
+      case ConditionTypes.NOT_EQUAL: result[condition.property] = {$ne: toScalar(condition.value)}; break;
+      case ConditionTypes.EMPTY: {
+        result.$or = [{}, {}];
+        result.$or[0][condition.property] = {$eq: null};
+        result.$or[1][condition.property] = {$eq: ''};
+      } break;
+      case ConditionTypes.NOT_EMPTY: {
+        result.$and = [{}, {}];
+        result.$and[0][condition.property] = {$ne: null};
+        result.$and[1][condition.property] = {$ne: ''};
+      } break;
+      case ConditionTypes.LIKE: result[condition.property] = {$regex: new RegExp(toScalar(condition.value))}; break;
+      case ConditionTypes.LESS: result[condition.property] = {$lt: toScalar(condition.value)}; break;
+      case ConditionTypes.MORE: result[condition.property] = {$gt: toScalar(condition.value)}; break;
+      case ConditionTypes.LESS_OR_EQUAL: result[condition.property] = {$lte: toScalar(condition.value)}; break;
+      case ConditionTypes.MORE_OR_EQUAL: result[condition.property] = {$gte: toScalar(condition.value)}; break;
+      case ConditionTypes.IN: result[condition.property] = {$in: condition.value}; break;
     }
-  } else if (condition.nestedConditions) {
+  } else {
+    var value = [];
+    for (var i = 0; i < condition.nestedConditions.length; i++) {
+      value[value.length] = ConditionParser(condition.nestedConditions[i]);
+    }
     switch (condition.operation) {
-      case OperationTypes.AND: return conjunct(item, condition.nestedConditions);
-      case OperationTypes.OR: return disjunct(item, condition.nestedConditions);
-      case OperationTypes.NOT: return !conjunct(item, condition.nestedConditions);
+      case OperationTypes.AND: result.$and = value; break;
+      case OperationTypes.OR: result.$or = value; break;
+      case OperationTypes.NOT: result.$not = {$and: value}; break;
     }
   }
-  return false;
+  return result;
 }
 
-function conjunct(item, conditions) {
-  if (Array.isArray(conditions) && conditions.length) {
-    for (var i = 0; i < conditions.length; i++) {
-      if (!checkCondition(item, conditions[i])) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-}
-
-function disjunct(item, conditions) {
-  if (Array.isArray(conditions) && conditions.length) {
-    for (var i = 0; i < conditions.length; i++) {
-      if (checkCondition(item, conditions[i])) {
-        return true;
-      }
-    }
-    return false;
-  }
-  return false;
-}
-
-/**
- * @param {Item} item
- * @param {Array} conditions
- */
-module.exports = function (item, conditions) {
-  return conjunct(item, conditions);
-};
+module.exports = ConditionParser;
