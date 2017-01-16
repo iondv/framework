aa65567a7750cb1cca6e29fa474b7cd6be1925c3
Krasilnikov Dan <dkrasilnikov@gmail.com>
2017-01-16 23:32:21 +1000

    #IONCORE-182 оптимизированы выборки объектов - не загружаем при обогащении то, что было загружено ранее.

--- core/impl/datarepository/ionDataRepository.js
+++ core/impl/datarepository/ionDataRepository.js
@@ -183,9 +183,10 @@ function IonDataRepository(options) {
    * @param {Item} item
    * @param {Property} property
    * @param {{}} attrs
+   * @param {{}} loaded
    */
-  function prepareRefEnrichment(item, property, attrs) {
-    var refc = _this.meta.getMeta(property.meta.refClass, null, item.classMeta.getNamespace());
+  function prepareRefEnrichment(item, property, attrs, loaded) {
+    var refc = property.meta._refClass;
     if (refc) {
       if (!attrs.hasOwnProperty(item.classMeta.getName() + '.' + property.getName())) {
         attrs[item.classMeta.getName() + '.' + property.getName()] = {
@@ -207,9 +208,14 @@ function IonDataRepository(options) {
       }
 
       if (v) {
-        attrs[item.classMeta.getName() + '.' + property.getName()].filter.push(v);
         if (typeof item.references === 'undefined') {
           item.references = {};
+        }
+        if (!property.meta.backRef && loaded.hasOwnProperty(refc.getCanonicalName() + '@' + v)) {
+          item.references[property.getName()] =
+            _this._wrap(refc.getCanonicalName(), loaded[refc.getCanonicalName() + '@' + v].base);
+        } else {
+          attrs[item.classMeta.getName() + '.' + property.getName()].filter.push(v);
         }
       }
     }
@@ -219,9 +225,11 @@ function IonDataRepository(options) {
    * @param {Item} item
    * @param {Property} property
    * @param {{}} attrs
+   * @param {{}} loaded
    */
-  function prepareColEnrichment(item, property, attrs) {
-    var refc = _this.meta.getMeta(property.meta.itemsClass, null, item.classMeta.getNamespace());
+  function prepareColEnrichment(item, property, attrs, loaded) {
+    var refc = property.meta._refClass;
+    item.collections = item.collections || {};
     if (refc) {
       if (!attrs.hasOwnProperty(item.classMeta.getName() + '.' + property.getName())) {
         attrs[item.classMeta.getName() + '.' + property.getName()] = {
@@ -243,13 +251,18 @@ function IonDataRepository(options) {
         }
       } else {
         var v = item.get(property.getName());
-        if (v) {
-          attrs[item.classMeta.getName() + '.' + property.getName()].colItems =
-            attrs[item.classMeta.getName() + '.' + property.getName()].colItems.concat(v);
+        if (Array.isArray(v)) {
+          item.collections[property.getName()] = [];
+          v.forEach(function (v) {
+            if (loaded.hasOwnProperty(refc.getCanonicalName() + '@' + v)) {
+              item.collections[property.getName()].push(
+                _this._wrap(refc.getCanonicalName(), loaded[refc.getCanonicalName() + '@' + v].base)
+              );
+            } else {
+              attrs[item.classMeta.getName() + '.' + property.getName()].colItems.push(v);
+            }
+          });
         }
-      }
-      if (typeof item.collections === 'undefined') {
-        item.collections = [];
       }
     }
   }
@@ -271,9 +284,10 @@ function IonDataRepository(options) {
    * @param {Item[]} src
    * @param {Number} depth
    * @param {String[][]} [forced]
+   * @param {{}} [loaded]
    * @returns {Promise}
    */
-  function enrich(src, depth, forced) {
+  function enrich(src, depth, forced, loaded) {
     return new Promise(function (resolve, reject) {
       var i, nm, attrs, item, props, promises, filter, cn, cm, forced2, pcl;
 
@@ -281,9 +295,14 @@ function IonDataRepository(options) {
       formForced(forced, forced2);
       attrs = {};
       promises = [];
+      loaded = loaded || {};
 
       try {
         pcl = {};
+        for (i = 0; i < src.length; i++) {
+          loaded[src[i].getClassName() + '@' + src[i].getItemId()] = src[i];
+        }
+
         for (i = 0; i < src.length; i++) {
           item = src[i];
           if (item && item.constructor.name === 'Item') {
@@ -300,9 +319,9 @@ function IonDataRepository(options) {
                   (forced2.hasOwnProperty(nm) || props[nm].eagerLoading()) && depth >= _this.maxEagerDepth
                 ) {
                   if (props[nm].getType() === PropertyTypes.REFERENCE) {
-                    prepareRefEnrichment(item, props[nm], attrs);
+                    prepareRefEnrichment(item, props[nm], attrs, loaded);
                   } else if (props[nm].getType() === PropertyTypes.COLLECTION && props[nm].eagerLoading()) {
-                    prepareColEnrichment(item, props[nm], attrs);
+                    prepareColEnrichment(item, props[nm], attrs, loaded);
                   }
                 }
               }
@@ -342,7 +361,8 @@ function IonDataRepository(options) {
                   {
                     filter: filter,
                     nestingDepth: depth - 1,
-                    forceEnrichment: forced2[attrs[nm].attrName]
+                    forceEnrichment: forced2[attrs[nm].attrName],
+                    ___loaded: loaded
                   }
                 )
               );
@@ -807,6 +827,7 @@ function IonDataRepository(options) {
    * @param {Boolean} [options.countTotal]
    * @param {Number} [options.nestingDepth]
    * @param {String[][]} [options.forceEnrichment]
+   * @param {{}} [options.___loaded]
    * @returns {Promise}
    */
   this._getList = function (obj, options) {
@@ -850,7 +871,12 @@ function IonDataRepository(options) {
     ).
     then(
       function (result) {
-        return enrich(result, options.nestingDepth ? options.nestingDepth : 0, options.forceEnrichment);
+        return enrich(
+          result,
+          options.nestingDepth ? options.nestingDepth : 0,
+          options.forceEnrichment,
+          options.___loaded
+        );
       }
     ).
     then(calcItemsProperties);
