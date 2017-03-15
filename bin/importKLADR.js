020721157bc3c8634b52a2321f5a162c41cff960
Данил Колесник <kalias_90@live.com>
2017-03-16 02:26:03 +1000

    #IONCORE-202 * Изменил определение формата импортируемого реестра, типа записи, её актуальности, набора данных, способ записи в соответствии с новой структурой

--- bin/importKLADR.js
+++ bin/importKLADR.js
@@ -14,12 +14,19 @@ const path = require('path');
 const Writable = require('stream').Writable;
 
 const classNames = {
+  SEXT: 'SEXT@develop-and-test',
+  EXTR: 'EXTR@develop-and-test',
   STREET: 'STREET@develop-and-test',
+  PLAN: 'PLAN@develop-and-test',
+  ADDROBJ_LOW: 'ADDROBJ_LOW@develop-and-test',
   PLACE: 'PLACE@develop-and-test',
+  CTAR: 'CTAR@develop-and-test',
   CITY: 'CITY@develop-and-test',
+  SETTLEMENT: 'SETTLEMENT@develop-and-test',
   AREA: 'AREA@develop-and-test',
+  AUTO: 'AUTO@develop-and-test',
   REGION: 'REGION@develop-and-test',
-  KLADR: 'KLADR@develop-and-test'
+  ADDROBJ_HIGHLEVEL: 'ADDROBJ_HIGHLEVEL@develop-and-test'
 };
 
 var sysLog = new IonLogger({});
@@ -27,6 +34,7 @@ var scope = null;
 var charset = 'cp866';
 var sourcePath = null;
 var regionFilter = null;
+var isFias = null;
 
 for (var i = 0; i < process.argv.length; i++) {
   if (process.argv[i] === '--sourcePath') {
@@ -35,7 +43,15 @@ for (var i = 0; i < process.argv.length; i++) {
     regionFilter = process.argv[i + 1];
   } else if (process.argv[i] === '--charset') {
     charset = process.argv[i + 1];
+  } else if (process.argv[i] === '--FIAS') {
+    isFias = true;
+  } else if (process.argv[i] === '--KLADR') {
+    isFias = false;
   }
+}
+
+if (isFias === null) {
+  isFias = fs.existsSync(path.join(sourcePath, 'ADDROBJ.DBF'));
 }
 
 function importFile(fn) {
@@ -52,6 +68,7 @@ function importFile(fn) {
         switch (parser.header.fields[i].name) {
           case 'NAME':
           case 'SOCR':
+          case 'OFFNAME':
           case 'FORMALNAME':
           case 'SHORTNAME': parser.header.fields[i].raw = true; break;
         }
@@ -60,26 +77,24 @@ function importFile(fn) {
       parser.stream.pipe(new Writable({
         write: function (record, encoding, cb) {
           importRecord(record)
-            .then(
-              function (result) {
-                if (result) {
-                  counter++;
-                }
-                gCounter++;
-                process.stdout.write('\rПрогресс импорта: ' + Math.round(gCounter / parser.header.numberOfRecords * 100) + '%');
-                cb();
+            .then(function (result) {
+              if (result) {
+                counter++;
               }
-            )
-            .catch(
-              function (err) {
-                console.error('\r' + err);
-                cb();
-              }
-            );
+              gCounter++;
+              process.stdout.write('\rПрогресс импорта: ' +
+                Math.round(gCounter / parser.header.numberOfRecords * 100) + '%');
+              cb();
+            })
+            .catch(function (err) {
+              console.error('\r' + err);
+              cb();
+            });
         },
         objectMode: true
       })).on('finish', function () {
-        console.log(`\nИз файла ${fn} импортировано ${counter} записей. Затрачено ${(new Date() - start) / 1000} секунд.`);
+        console.log(`\nИз файла ${fn} импортировано ${counter} записей.` +
+          `Затрачено ${(new Date() - start) / 1000} секунд.`);
         resolve();
       });
     });
@@ -93,20 +108,15 @@ di('app', config.di,
 ).then(function (s) {
   scope = s;
   var p;
-  fs.readdirSync(sourcePath).forEach(function (file) {
-    if (file === 'KLADR.DBF' || file === 'STREET.DBF' || file === 'ADDROBJ.DBF') {
-      if (p) {
-        p = p.then(importFile(path.join(sourcePath, file)));
-      } else {
-        p = importFile(path.join(sourcePath, file))();
-      }
-    }
-  });
-  if (p) {
-    return p;
+  if (isFias) {
+    p = importFile(path.join(sourcePath, 'ADDROBJ.DBF'))();
+  } else if (!isFias) {
+    p = importFile(path.join(sourcePath, 'KLADR.DBF'))()
+      .then(importFile(path.join(sourcePath, 'STREET.DBF')));
   } else {
-    return Promise.reject(new Error('Указанная директория не содержит необходимых для импорта .DBF-файлов формата КЛАДР либо ФИАС.'));
+    return Promise.reject(new Error('Не удаётся определить формат импортируемого реестра адресов.'));
   }
+  return p;
 }).then(function () {
   console.log('Проверка ссылочной целостности адресных элементов');
   return checkContainers(classNames.KLADR, scope.dataRepo, scope.metaRepo);
@@ -127,26 +137,36 @@ di('app', config.di,
 });
 
 function importRecord(record) {
-  var fias = record.hasOwnProperty('NORMDOC');
-  var className = getRecordClass(record, fias);
-  if (
-    className &&
-    (fias && record.ACTSTATUS === '1' || !fias && record.CODE.substring(record.CODE.length - 2) === '00') &&
-    (!regionFilter || record.CODE.substring(0, 2) === regionFilter)
-  ) {
-    return scope.dataRepo.saveItem(
-      className,
-      null,
-      getData(record, className, fias),
-      null,
-      null,
-      {
-        skipResult: true,
-        nestingDepth: 0,
-        autoAssign: true,
-        ignoreIntegrityCheck: true
+  if (isActual(record) && isFiltered(record)) {
+    var className = getRecordClass(record);
+    var options = {
+      count: 1,
+      nestingDepth: 0,
+      skipResult: true,
+      ignoreIntegrityCheck: true,
+      filter: []
+    };
+    if (isFias) {
+      options.filter.push({AOGUID: {$eq: record.AOGUID}});
+    } else {
+      options.filter.push({CODE: {$eq: record.CODE}});
+    }
+    return scope.dataRepo.getList(className, options).then(function(item) {
+      var dummy = getData(record);
+      if (item) {
+        for (var val in item.base) {
+          if (item.base.hasOwnProperty(val) && dummy.hasOwnProperty(val) && item.base[val] === dummy[val]) {
+            delete dummy[val];
+          }
+        }
+        if (Object.keys(dummy).length === 0 && dummy.constructor === Object) {
+          return scope.dataRepo.editItem(className, item.getItemId(), dummy);
+        }
+      } else {
+        return scope.dataRepo.createItem(className, dummy);
       }
-    );
+      return Promise.resolve();
+    });
   }
   return Promise.resolve();
 }
@@ -166,26 +186,33 @@ function checkContainers(className, dataRepo) {
   });
 }
 
-function getRecordClass(record, fias) {
-  if (fias) {
-    if (record.STREETCODE && record.STREETCODE !== '0000') {
-      return classNames.STREET;
-    } else if (record.PLANCODE && record.PLANCODE !== '0000') {
-      return null; // TODO: Код элемента планировочной структуры
-    } else if (record.PLACECODE && record.PLACECODE !== '000') {
-      return classNames.PLACE;
-    } else if (record.CTARCODE && record.CTARCODE !== '000') {
-      return null; // TODO: Код внутригородского района
-    } else if (record.CITYCODE && record.CITYCODE !== '000') {
-      return classNames.CITY;
-    } else if (record.AREACODE && record.AREACODE !== '000') {
-      return classNames.AREA;
-    } else if (record.AUTOCODE && record.AUTOCODE !== '000') {
-      return null; // TODO: уровень автономного округа (устаревшее)
-    } else if (record.REGIONCODE && record.REGIONCODE !== '00') {
-      return classNames.REGION;
+function isActual(record) {
+  return isFias && record.LIVESTATUS === '1' && record.ACTSTATUS === '1' ||
+    !isFias && record.CODE.substring(record.CODE.length - 2) === '00';
+}
+
+function isFiltered(record) {
+  return !regionFilter ||
+    isFias && record.REGIONCODE === regionFilter ||
+    !isFias && record.CODE.substring(0, 2) === regionFilter;
+}
+
+function getRecordClass(record) {
+  if (isFias) {
+    switch (record.AOLEVEL) {
+      case '1':   return classNames.REGION;
+      case '2':   return classNames.AUTO;
+      case '3':   return classNames.AREA;
+      case '35':  return classNames.SETTLEMENT;
+      case '4':   return classNames.CITY;
+      case '5':   return classNames.CTAR;
+      case '6':   return classNames.PLACE;
+      case '65':  return classNames.PLAN;
+      case '7':   return classNames.STREET;
+      case '90':  return classNames.EXTR;
+      case '91':  return classNames.SEXT;
     }
-  } else {
+  } else if (!isFias) {
     if (record.CODE.length === 13) {
       if (record.CODE.substring(2, 11) === '000000000') {
         return classNames.REGION;
@@ -200,44 +227,29 @@ function getRecordClass(record, fias) {
       return classNames.STREET;
     }
   }
-  return null;
+  throw new Error('Не удаётся определить тип записи ' + record.CODE + '.');
 }
 
-function getData(record, className, fias) {
+function getData(record, className) {
   var data = {};
-
-  if (fias) {
-    data.CODE = record.REGIONCODE + record.AREACODE + record.CITYCODE + record.PLACECODE;
-    if (className === classNames.STREET) {
-      data.CODE = data.CODE + record.STREETCODE;
-    }
-  } else {
-    data.CODE = record.CODE.substring(0, record.CODE.length - 2);
+  if (isFias) {
+    data.ID = record.AOGUID;
+    data.AOGUID = record.AOGUID;
+    data.CODE = record.CODE;
+    data.SHORTNAME = convertCharset(record.SHORTNAME);
+    data.OFFNAME = convertCharset(record.OFFNAME);
+    data.FORMALNAME = convertCharset(record.FORMALNAME);
+    data.OKATO = record.OKATO;
+    data.OKTMO = record.OKTMO;
+    data.POSTALCODE = record.POSTALCODE;
+    data.CONTAINER = record.PARENTGUID;
+  } else if (!isFias) {
+    data.CODE = record.CODE;
+    data.SHORTNAME = convertCharset(record.SOCR);
+    data.OFFNAME = convertCharset(record.NAME);
+    data.OKATO = record.OCATD;
+    data.POSTALCODE = record.INDEX;
   }
-
-  switch (className) {
-    case classNames.STREET: {data.CONTAINER = data.CODE.substring(0, 11);} break;
-    case classNames.PLACE: {data.CONTAINER = data.CODE.substring(0, 8) + '000';} break;
-    case classNames.CITY: {data.CONTAINER = data.CODE.substring(0, 5) + '000000';} break;
-    case classNames.AREA: {data.CONTAINER = data.CODE.substring(0, 2) + '000000000';} break;
-  }
-
-  if (fias) {
-    data.NAME = convertCharset(record.FORMALNAME);
-    data.SOCR = convertCharset(record.SHORTNAME);
-    data.INDEX = record.POSTALCODE;
-    data.GNINMB = record.IFNSUL.length > 0 ? record.IFNSUL : record.IFNSFL;
-    data.UNO = record.TERRIFNSUL.length > 0 ? record.TERRIFNSUL : record.TERRIFNSFL;
-    data.OCATD = record.OKATO;
-  } else {
-    data.NAME = convertCharset(record.NAME);
-    data.SOCR = convertCharset(record.SOCR);
-    data.INDEX = record.INDEX;
-    data.GNINMB = record.GNINMB;
-    data.UNO = record.UNO;
-    data.OCATD = record.OCATD;
-  }
-
   return data;
 }
 
