f648c558146064393df63ba67ef347d8aa118b83
Данил Колесник <kalias_90@live.com>
2017-03-17 03:53:30 +1000

    #IONCORE-202 * Исправил недочеты * Описал удаление переподчиненых записей (новые сами запишутся, а удаление неактуальных позволит определить неправильные ссылки) * Описал переподчинение ссылок на контейнер на основе ALTNAMES.DBF * Описал стадию определения и установки контейнеров для КЛАДРа на основе поля CODE * Усовершенствовал проверку ссылочной целостности

--- bin/importKLADR.js
+++ bin/importKLADR.js
@@ -18,7 +18,7 @@ const classNames = {
   EXTR: 'EXTR@develop-and-test',
   STREET: 'STREET@develop-and-test',
   PLAN: 'PLAN@develop-and-test',
-  ADDROBJ_LOW: 'ADDROBJ_LOW@develop-and-test',
+  ADDROBJ_LOWLEVEL: 'ADDROBJ_LOWLEVEL@develop-and-test',
   PLACE: 'PLACE@develop-and-test',
   CTAR: 'CTAR@develop-and-test',
   CITY: 'CITY@develop-and-test',
@@ -51,54 +51,11 @@ for (var i = 0; i < process.argv.length; i++) {
 }
 
 if (isFias === null) {
-  isFias = fs.existsSync(path.join(sourcePath, 'ADDROBJ.DBF'));
-}
-
-function importFile(fn) {
-  return function () {
-    return new Promise(function (resolve) {
-      var start = new Date();
-      var counter = 0;
-      var gCounter = 0;
-      var parser = new DBF(fn, {parseTypes: false});
-
-      console.log(`Читается файл ${fn}. Он содержит ${parser.header.numberOfRecords} записей.`);
-
-      for (var i = 0; i < parser.header.fields.length; i++) {
-        switch (parser.header.fields[i].name) {
-          case 'NAME':
-          case 'SOCR':
-          case 'OFFNAME':
-          case 'FORMALNAME':
-          case 'SHORTNAME': parser.header.fields[i].raw = true; break;
-        }
-      }
-
-      parser.stream.pipe(new Writable({
-        write: function (record, encoding, cb) {
-          importRecord(record)
-            .then(function (result) {
-              if (result) {
-                counter++;
-              }
-              gCounter++;
-              process.stdout.write('\rПрогресс импорта: ' +
-                Math.round(gCounter / parser.header.numberOfRecords * 100) + '%');
-              cb();
-            })
-            .catch(function (err) {
-              console.error('\r' + err);
-              cb();
-            });
-        },
-        objectMode: true
-      })).on('finish', function () {
-        console.log(`\nИз файла ${fn} импортировано ${counter} записей.` +
-          `Затрачено ${(new Date() - start) / 1000} секунд.`);
-        resolve();
-      });
-    });
-  };
+  if (fs.existsSync(path.join(sourcePath, 'ADDROBJ.DBF'))) {
+    isFias = true;
+  } else if (fs.existsSync(path.join(sourcePath, 'KLADR.DBF'))) {
+    isFias = false;
+  }
 }
 
 di('app', config.di,
@@ -107,22 +64,33 @@ di('app', config.di,
   ['auth', 'rtEvents', 'sessionHandler']
 ).then(function (s) {
   scope = s;
-  var p;
-  if (isFias) {
-    p = importFile(path.join(sourcePath, 'ADDROBJ.DBF'))();
-  } else if (!isFias) {
-    p = importFile(path.join(sourcePath, 'KLADR.DBF'))()
-      .then(importFile(path.join(sourcePath, 'STREET.DBF')));
+  if (isFias === true) {
+    return importFile(path.join(sourcePath, 'ADDROBJ.DBF'), importRecord);
+  } else if (isFias === false) {
+    return importFile(path.join(sourcePath, 'KLADR.DBF'), importRecord)
+      .then(function () {
+        return importFile(path.join(sourcePath, 'STREET.DBF'), importRecord);
+      });
   } else {
     return Promise.reject(new Error('Не удаётся определить формат импортируемого реестра адресов.'));
   }
-  return p;
 }).then(function () {
-  console.log('Проверка ссылочной целостности адресных элементов');
-  return checkContainers(classNames.KLADR, scope.dataRepo, scope.metaRepo);
+  if (isFias === false) {
+    // TODO: Решить вопрос с ссылочностью на нижнем уровне
+    return importFile(
+      path.join(sourcePath, 'ALTNAMES.DBF'),
+      reassign
+    ).then(function () {
+      return setContainers(classNames.ADDROBJ_HIGHLEVEL);
+    }).then(function () {
+      return setContainers(classNames.ADDROBJ_LOWLEVEL);
+    });
+  }
+  return Promise.resolve();
 }).then(function () {
-  console.log('Проверка ссылочной целостности улиц');
-  return checkContainers(classNames.STREET, scope.dataRepo, scope.metaRepo);
+  return checkContainers(classNames.ADDROBJ_HIGHLEVEL);
+}).then(function () {
+  return checkContainers(classNames.ADDROBJ_LOWLEVEL);
 }).then(function () {
   return scope.dataSources.disconnect();
 }).then(
@@ -136,69 +104,241 @@ di('app', config.di,
   scope.dataSources.disconnect().then(exit).catch(exit);
 });
 
+function importFile(fn, recordCallback) {
+  return new Promise(function (resolve) {
+    var start = new Date();
+    var counter = 0;
+    var gCounter = 0;
+    var parser = new DBF(fn, {parseTypes: false});
+    console.log(`Читается файл ${fn}. Он содержит ${parser.header.numberOfRecords} записей.`);
+
+    for (var i = 0; i < parser.header.fields.length; i++) {
+      switch (parser.header.fields[i].name) {
+        case 'NAME':
+        case 'SOCR':
+        case 'OFFNAME':
+        case 'FORMALNAME':
+        case 'SHORTNAME': parser.header.fields[i].raw = true; break;
+      }
+    }
+    parser.stream.pipe(new Writable({
+      write: function (record, encoding, cb) {
+        recordCallback(record).then(function (result) {
+          if (result) { counter++; }
+          gCounter++;
+          process.stdout.write('\rПрогресс: ' + Math.round(gCounter / parser.header.numberOfRecords * 100) + '%');
+          cb();
+        }).catch(function (err) {
+          console.error('\r' + err);
+          cb();
+        });
+      },
+      objectMode: true
+    })).on('finish', function () {
+      console.log(`\nОбработано ${counter} записей. Затрачено ${(new Date() - start) / 1000} секунд.`);
+      resolve();
+    });
+  });
+}
+
 function importRecord(record) {
   if (isActual(record) && isFiltered(record)) {
     var className = getRecordClass(record);
     var options = {
       count: 1,
-      nestingDepth: 0,
-      skipResult: true,
-      ignoreIntegrityCheck: true,
-      filter: []
+      nestingDepth: 0
     };
-    if (isFias) {
-      options.filter.push({AOGUID: {$eq: record.AOGUID}});
+    if (isFias === true) {
+      options.filter = {FIAS_AOGUID: {$eq: record.AOGUID}};
+    } else if (isFias === false) {
+      options.filter = {KLADR_CODE: {$eq: record.CODE}};
     } else {
-      options.filter.push({CODE: {$eq: record.CODE}});
+      return Promise.reject(new Error('Не удаётся определить формат импортируемой записи.'));
     }
-    return scope.dataRepo.getList(className, options).then(function(item) {
+    return scope.dataRepo.getList(className, options).then(function (items) {
+      var item = null;
+      if (Array.isArray(items) && items.length > 0) {
+        item = items[0];
+      }
       var dummy = getData(record);
+      options = {
+        skipResult: true,
+        ignoreIntegrityCheck: true
+      };
       if (item) {
         for (var val in item.base) {
           if (item.base.hasOwnProperty(val) && dummy.hasOwnProperty(val) && item.base[val] === dummy[val]) {
             delete dummy[val];
           }
         }
-        if (Object.keys(dummy).length === 0 && dummy.constructor === Object) {
-          return scope.dataRepo.editItem(className, item.getItemId(), dummy);
+        if (Object.keys(dummy).length > 0) {
+          return scope.dataRepo.editItem(className, item.getItemId(), dummy, null, options);
         }
       } else {
-        return scope.dataRepo.createItem(className, dummy);
+        if (Object.keys(dummy).length > 0) {
+          return scope.dataRepo.createItem(className, dummy, null, null, options);
+        }
       }
       return Promise.resolve();
+    });
+  } else if (isFias === false && record.CODE.substring(record.CODE.length - 2) === '51') {
+    return scope.dataRepo.getList(getRecordClass(record), {
+      count: 1,
+      nestingDepth: 0,
+      filter: {KLADR_CODE: {$eq: record.CODE.substring(0, 11) + '00'}}
+    }).then(function (results) {
+      var p = new Promise(function (r) {r();});
+      results.forEach(function (item) {
+        p = p.then(function () {
+          return scope.dataRepo.deleteItem(item.getClassName(), item.getItemId());
+        });
+      });
+      return p;
     });
   }
   return Promise.resolve();
 }
 
-function checkContainers(className, dataRepo) {
-  return dataRepo.getList(className, {
+function setContainers(className) {
+  return scope.dataRepo.getList(className, {
     filter: {$and: [
-      {CONTAINER: {$empty: false}},
-      {'CONTAINER.CODE': {$empty: true}}
+      {_class: {$ne: classNames.REGION}},
+      {CONTAINER: {$empty: true}}
     ]},
     nestingDepth: 0
-  }).then(function (result) {
-    for (var i = 0; i < result.length; i++) {
-      console.error(`Запись ${result[i].base.CODE} ссылается на несуществующий контейнер ${result[i].base.CONTAINER}`);
+  }).then(function (results) {
+    var p = new Promise(function (r) {r();});
+    results.forEach(function (item) {
+      p = p.then(function () {
+        var options = {
+          count: 1,
+          nestingDepth: 0
+        };
+        var code = null;
+        switch (item.getClassName()) {
+          case classNames.AREA: code = item.base.KLADR_CODE.substring(0, 2) + '00000000000'; break;
+          case classNames.CITY: code = item.base.KLADR_CODE.substring(0, 5) + '00000000'; break;
+          case classNames.PLACE: code = item.base.KLADR_CODE.substring(0, 8) + '00000'; break;
+          case classNames.STREET: code = item.base.KLADR_CODE.substring(0, 11) + '00'; break;
+          default: return Promise.resolve();
+        }
+        options.filter = {KLADR_CODE: {$eq: code}};
+        return scope.dataRepo.getList(classNames.ADDROBJ_HIGHLEVEL, options)
+          .then(function (containers) {
+            var container = null;
+            if (Array.isArray(containers) && containers.length > 0) {
+              container = containers[0];
+            }
+            return scope.dataRepo.editItem(
+              item.getClassName(),
+              item.getItemId(),
+              {CONTAINER: container.getItemId()},
+              null,
+              {
+                skipResult: true,
+                ignoreIntegrityCheck: true
+              });
+          });
+      });
+    });
+    return p;
+  });
+}
+
+function reassign(record) {
+  var className = classNameByFiasCode(record.NEWCODE);
+  return scope.dataRepo.getList(className, {
+    count: 1,
+    nestingDepth: 0,
+    filter: {KLADR_CODE: {$regex: '^' + record.NEWCODE.substring(0, record.NEWCODE.length - 2)}}
+  }).then(function (containers) {
+    var container = null;
+    if (Array.isArray(containers) && containers.length > 0) {
+      container = containers[0];
     }
-    return result;
+    if (container) {
+      var prefix = null;
+      switch (classNameByFiasCode(record.OLDCODE)) {
+        case classNames.REGION: prefix = record.OLDCODE.substring(0, 2); break;
+        case classNames.AREA: prefix = record.OLDCODE.substring(0, 5); break;
+        case classNames.CITY: prefix = record.OLDCODE.substring(0, 8); break;
+        case classNames.PLACE: prefix = record.OLDCODE.substring(0, 11); break;
+      }
+      return resetContainers(classNames.ADDROBJ_HIGHLEVEL, container, prefix).then(function () {
+        return resetContainers(classNames.ADDROBJ_LOWLEVEL, container, prefix);
+      });
+    } else {
+      return Promise.reject(new Error(`Не удаётся найти переподчиненную запись ${record.NEWCODE}.`));
+    }
+  });
+}
+
+function resetContainers(className, container, prefix) {
+  return scope.dataRepo.getList(className, {
+    nestingDepth: 0,
+    filter: {KLADR_CODE: {$regex: '^' + prefix}}
+  }).then(function (items) {
+    var p = new Promise(function (r) {r();});
+    items.forEach(function (item) {
+      p = p.then(function () {
+        return scope.dataRepo.editItem(
+          item.getClassName(),
+          item.getItemId(),
+          {CONTAINER: container.getItemId()},
+          null,
+          {
+            skipResult: true,
+            ignoreIntegrityCheck: true
+          });
+      });
+    });
+    return p;
+  });
+}
+
+function checkContainers(className) {
+  return scope.dataRepo.getList(className, {
+    filter: {$and: [
+      {_class: {$ne: classNames.REGION}},
+      {CONTAINER: {$empty: true}}
+    ]},
+    nestingDepth: 0
+  }).then(function (results) {
+    results.forEach(function (result) {
+      console.error(`Запись ${result.getItemId()}, ` +
+        `принадлежащая классу ${result.getClassName()}, не имеет ссылки на контейнер.`);
+    });
+    return Promise.resolve();
+  }).then(function () {
+    return scope.dataRepo.getList(className, {
+      filter: {$and: [
+        {CONTAINER: {$empty: false}},
+        {'CONTAINER.CODE': {$empty: true}}
+      ]},
+      nestingDepth: 0
+    });
+  }).then(function (results) {
+    results.forEach(function (result) {
+      console.error(`Запись ${result.getItemId()} ссылается на несуществующий контейнер ${result.base.CONTAINER}.`);
+    });
+    return Promise.resolve();
   });
 }
 
 function isActual(record) {
-  return isFias && record.LIVESTATUS === '1' && record.ACTSTATUS === '1' ||
-    !isFias && record.CODE.substring(record.CODE.length - 2) === '00';
+  return isFias === true && record.LIVESTATUS === '1' && record.ACTSTATUS === '1' ||
+    isFias === false &&
+    (record.CODE.substring(record.CODE.length - 2) === '00' || record.CODE.substring(record.CODE.length - 2) === '99');
 }
 
 function isFiltered(record) {
   return !regionFilter ||
-    isFias && record.REGIONCODE === regionFilter ||
-    !isFias && record.CODE.substring(0, 2) === regionFilter;
+    isFias === true && record.REGIONCODE === regionFilter ||
+    isFias === false && record.CODE.substring(0, 2) === regionFilter;
 }
 
 function getRecordClass(record) {
-  if (isFias) {
+  if (isFias === true) {
     switch (record.AOLEVEL) {
       case '1':   return classNames.REGION;
       case '2':   return classNames.AUTO;
@@ -212,30 +352,34 @@ function getRecordClass(record) {
       case '90':  return classNames.EXTR;
       case '91':  return classNames.SEXT;
     }
-  } else if (!isFias) {
-    if (record.CODE.length === 13) {
-      if (record.CODE.substring(2, 11) === '000000000') {
-        return classNames.REGION;
-      } else if (record.CODE.substring(5, 11) === '000000') {
-        return classNames.AREA;
-      } else if (record.CODE.substring(8, 11) === '000') {
-        return classNames.CITY;
-      } else {
-        return classNames.PLACE;
-      }
-    } else if (record.CODE.length === 17) {
-      return classNames.STREET;
-    }
+  } else if (isFias === false) {
+    return classNameByFiasCode(record.CODE);
   }
-  throw new Error('Не удаётся определить тип записи ' + record.CODE + '.');
+  throw new Error('Не удаётся определить тип записи.');
 }
 
-function getData(record, className) {
+function classNameByFiasCode(code) {
+  if (code.length === 13) {
+    if (code.substring(2, 11) === '000000000') {
+      return classNames.REGION;
+    } else if (code.substring(5, 11) === '000000') {
+      return classNames.AREA;
+    } else if (code.substring(8, 11) === '000') {
+      return classNames.CITY;
+    } else {
+      return classNames.PLACE;
+    }
+  } else if (code.length === 17) {
+    return classNames.STREET;
+  }
+}
+
+function getData(record) {
   var data = {};
-  if (isFias) {
+  if (isFias === true) {
     data.ID = record.AOGUID;
-    data.AOGUID = record.AOGUID;
-    data.CODE = record.CODE;
+    data.FIAS_AOGUID = record.AOGUID;
+    data.KLADR_CODE = record.CODE;
     data.SHORTNAME = convertCharset(record.SHORTNAME);
     data.OFFNAME = convertCharset(record.OFFNAME);
     data.FORMALNAME = convertCharset(record.FORMALNAME);
@@ -243,8 +387,8 @@ function getData(record, className) {
     data.OKTMO = record.OKTMO;
     data.POSTALCODE = record.POSTALCODE;
     data.CONTAINER = record.PARENTGUID;
-  } else if (!isFias) {
-    data.CODE = record.CODE;
+  } else if (isFias === false) {
+    data.KLADR_CODE = record.CODE;
     data.SHORTNAME = convertCharset(record.SOCR);
     data.OFFNAME = convertCharset(record.NAME);
     data.OKATO = record.OCATD;
