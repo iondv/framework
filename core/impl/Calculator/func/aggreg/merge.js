14d49154d69b5d8de5158a5e1f1d363e7f562317
Krasilnikov Dan <dkrasilnikov@gmail.com>
2017-07-25 23:31:08 +1000

    #IONCORE-277 в функцию merge добавлен параметр unique логика merge и sum теперь учитывает уникальность на всех уровнях разименования атрибутов исправлена формула в классе pochtamt

--- core/impl/Calculator/func/aggreg/merge.js
+++ core/impl/Calculator/func/aggreg/merge.js
@@ -4,33 +4,52 @@
 'use strict';
 const c = require('./oper');
 const Item = require('core/interfaces/DataRepository').Item;
-// jshint maxcomplexity: 20
+const p = require('./processed');
+
+// jshint maxcomplexity: 20, maxstatements: 30
+
+function work(col, attr, cond, unique, sep) {
+  let result = '';
+  sep = sep || ' ';
+  if (Array.isArray(col)) {
+    let processed = p();
+    for (let i = 0; i < col.length; i++) {
+      if (col[i] !== null) {
+        if (cond) {
+          if (!cond.apply(col[i])) {
+            continue;
+          }
+        }
+
+        if (unique && processed(col[i])) {
+          continue;
+        }
+
+        let v = null;
+        if (attr.indexOf('.') > 0) {
+          let att = attr.substr(0, attr.indexOf('.'));
+          let satt = attr.substr(attr.indexOf('.') + 1);
+          v = col[i] instanceof Item ? col[i].property(att).evaluate() : col[i][att];
+          if (!Array.isArray(v)) {
+            v = [v];
+          }
+          v = work(v, satt, null, unique, sep);
+        } else {
+          v = col[i] instanceof Item ? col[i].get(attr) : col[i][attr];
+        }
+
+        if (v) {
+          result = result + (result ? sep : '') + v;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+
 /**
  * @param {DataRepository} dataRepo
  * @returns {Function}
  */
-module.exports = c(
-  function (col, attr, cond, sep) {
-    let result = '';
-    if (Array.isArray(col)) {
-      for (let i = 0; i < col.length; i++) {
-        if (col[i] !== null) {
-          if (cond) {
-            if (!cond.apply(col[i])) {
-              continue;
-            }
-          }
-          let v = col[i] instanceof Item ? col[i].get(attr) : col[i][attr];
-          if (v) {
-            if (Array.isArray(v)) {
-              v = v.join(sep || ' ');
-            }
-            result = result + v + (sep || ' ');
-          }
-        }
-      }
-    }
-    return result;
-  },
-  'sum'
-);
+module.exports = c(work, 'sum');
--- core/impl/Calculator/func/aggreg/sum.js
+++ core/impl/Calculator/func/aggreg/sum.js
@@ -6,35 +6,47 @@ const c = require('./oper');
 const Item = require('core/interfaces/DataRepository').Item;
 const p = require('./processed');
 
+// jshint maxstatements: 30, maxcomplexity: 20
+function work(col, attr, cond, unique) {
+  let result = 0;
+  let processed = p();
+  if (Array.isArray(col)) {
+    for (let i = 0; i < col.length; i++) {
+      if (col[i] !== null) {
+        if (cond) {
+          if (!cond.apply(col[i])) {
+            continue;
+          }
+        }
+
+        if (unique && processed(col[i])) {
+          continue;
+        }
+
+        let v = null;
+        if (attr.indexOf('.') > 0) {
+          let att = attr.substr(0, attr.indexOf('.'));
+          let satt = attr.substr(attr.indexOf('.') + 1);
+          v = col[i] instanceof Item ? col[i].property(att).evaluate() : col[i][att];
+          if (!Array.isArray(v)) {
+            v = [v];
+          }
+          v = work(v, satt, null, unique);
+        } else {
+          v = col[i] instanceof Item ? col[i].get(attr) : col[i][attr];
+        }
+
+        if (v !== null) {
+          result = result + v;
+        }
+      }
+    }
+  }
+  return result;
+}
+
 /**
  * @param {DataRepository} dataRepo
  * @returns {Function}
  */
-module.exports = c(
-  function (col, attr, cond, unique) {
-    let result = 0;
-    let processed = p();
-    if (Array.isArray(col)) {
-      for (let i = 0; i < col.length; i++) {
-        if (col[i] !== null) {
-          if (cond) {
-            if (!cond.apply(col[i])) {
-              continue;
-            }
-          }
-
-          if (unique && processed(col[i])) {
-            continue;
-          }
-
-          let v = col[i] instanceof Item ? col[i].get(attr) : col[i][attr];
-          if (v) {
-            result = result + v;
-          }
-        }
-      }
-    }
-    return result;
-  },
-  'sum'
-);
+module.exports = c(work, 'sum');
