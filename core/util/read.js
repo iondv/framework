709572f2b415fd4ecb0a6c62b6598f0fdf4863b7
Vasiliy Ermilov <ermilov.work@yandex.ru>
2017-07-31 17:07:03 +1000

    MODREPORT-38

--- core/util/read.js
+++ core/util/read.js
@@ -49,51 +49,72 @@ function readJSON(filePath) {
 }
 module.exports.readJSON = readJSON;
 
-function readConfigFiles(dir) {
-  try {
+function processDirAsync(dir) {
+  return new Promise(function (resolve, reject) {
+    fs.access(dir, fs.R_OK, function (err) {
+      if (err) {
+        return reject(err);
+      }
+      fs.readdir(dir, function (err, files) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(files.map(f => path.join(dir, f)));
+      });
+    });
+  });
+}
+module.exports.processDirAsync = processDirAsync;
+
+function readConfigFiles(filesList) {
+  return new Promise(function (resolve, reject) {
     let promises = [];
     let files = {};
-    processDir(
-      dir,
-      nm => ['.json', '.yml'].indexOf(path.extname(nm)) > -1,
-      fn => {
+
+    if (Array.isArray(filesList)) {
+      filesList.forEach(fn => {
         let ext = path.extname(fn);
-        let fname = path.basename(fn, ext);
-        if (!files[fname]) {
-          files[fname] = {};
+        if (['.json', '.yml'].indexOf(ext) > -1) {
+          let fname = path.basename(fn, ext);
+          if (!files[fname]) {
+            files[fname] = {};
+          }
+          files[fname][ext] = fn;
         }
-        files[fname][ext] = fn;
-      }
-    );
+      });
+    }
+
     Object.keys(files).forEach(fname => {
       promises.push(new Promise(function (resolve, reject) {
-        let yamlCfg = files[fname]['.yml'];
-        let jsonCfg = files[fname]['.json'];
-        let readers = [];
-        if (yamlCfg) {
-          readers.push(
-            readYAML(yamlCfg)
-            .catch(e => reject(new Error(`Не удалось прочитать содержимое файла ${yamlCfg}`)))
-          );
-        } else {
-          readers.push(Promise.resolve({}));
-        }
-        if (jsonCfg) {
-          readers.push(
-            readJSON(jsonCfg)
-            .catch(e => reject(new Error(`Не удалось прочитать содержимое файла ${jsonCfg}`)))
-          );
-        } else {
-          readers.push(Promise.resolve({}));
-        }
+        let readers = [
+          files[fname]['.yml'] ? readYAML(files[fname]['.yml']) : Promise.resolve({}),
+          files[fname]['.json'] ? readJSON(files[fname]['.json']) : Promise.resolve({})
+        ];
         Promise.all(readers)
           .then(data => resolve(merge(data[0], data[1])))
           .catch(e => reject(new Error(`Не удалось получить конфигурацию ${fname}`)));
       }));
     });
-    return Promise.all(promises);
-  } catch (err) {
-    return Promise.reject(err);
-  }
+    return Promise.all(promises).then(resolve).catch(reject);
+  });
 }
 module.exports.readConfigFiles = readConfigFiles;
+
+function access(path) {
+  try {
+    fs.accessSync(path, fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function readConfig(filePath) {
+  let params = path.parse(filePath);
+  let ymlFilePath = params.ext === '.yml' ? filePath : path.join(params.dir, params.name + '.yml');
+  let jsonFilePath = params.ext === '.json' ? filePath : path.join(params.dir, params.name + '.json');
+  let ymlFile = access(ymlFilePath) ? yaml.safeLoad(fs.readFileSync(ymlFilePath, 'utf-8'), 'utf-8') : {};
+  let jsonFile = access(jsonFilePath) ? JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8')) : {};
+  return merge(ymlFile, jsonFile);
+}
+module.exports.readConfig = readConfig;
