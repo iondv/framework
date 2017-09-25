03aa25afbebd695ae827976d3fe6e492abbd7ece
Krasilnikov Dan <dkrasilnikov@gmail.com>
2017-09-25 13:35:01 +1000

    #KHVMONUSL-27 Функция element в вычисляемых выражениях. Багфикс в модуле soap. Допилы в проекте мониторинга услуг.

---
+++ core/impl/Calculator/func/arrays/element.js
@@ -0,0 +1,28 @@
+/**
+ * Created by kras on 25.09.17.
+ */
+'use strict';
+const calc = require('../util').calculate;
+
+module.exports = function (args) {
+  return function () {
+   return calc(this, args, null,
+     function (args) {
+       let arr = [];
+       let ind = 0;
+       if (args.length > 0) {
+         if (Array.isArray(args[0])) {
+           arr = args[0];
+         }
+       }
+       if (args.length > 1) {
+         ind = args[1];
+       }
+       if (typeof arr[ind] !== 'undefined') {
+         return arr[ind];
+       }
+       return null;
+     }
+   );
+  };
+};
