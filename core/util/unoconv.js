9b01bc46e0419603dc0e614c738aa1b56e84efa8
Vasiliy Ermilov <ermilov.work@yandex.ru>
2018-01-21 23:03:01 +1000

    KHVGOSEKSP-473

--- core/util/unoconv.js
+++ core/util/unoconv.js
@@ -2,11 +2,105 @@
 
 const childProcess = require('child_process');
 const mime = require('mime');
+const {Readable} = require('stream');
 
 const unoconv = exports = module.exports = {};
+/*
+  Let stderr = [];
+
+  cp.stdout.on('data', (data) => {
+    console.log(data);
+    rs.push(data);
+  });
+
+  cp.stderr.on('data', (data) => {
+    console.log('err', data);
+    stderr.push(data);
+  });
+
+  cp.on('exit', () => {
+    if (stderr.length) {
+      console.log(Buffer.concat(stderr).toString());
+      return rs.emit('error', new Error(Buffer.concat(stderr).toString()));
+    }
+    rs.push(null);
+  });*/
+function readStream(cp) {
+  let missedErrors = [];
+  let stderr = [];
+
+  function onMissedError(err) {
+    missedErrors.push(err);
+  }
+
+  function onStdErr(chunk) {
+    stderr.push(chunk);
+  }
+
+  cp.stdout.on('error', onMissedError);
+  cp.stdout.on('data', (data) => {console.log(data);});
+  cp.stderr.on('data', onStdErr);
+
+  return function () {
+    return new Promise((resolve, reject) => {
+      let error = missedErrors.shift();
+      if (error) {
+        cp.stdout.removeListener('error', onMissedError);
+        return reject(error);
+      };
+      if (stderr.length) {
+        cp.stderr.removeListener('data', onStdErr);
+        return reject(new Error(Buffer.concat(stderr).toString()));
+      }
+
+      cp.stdout.on('data', ondata);
+      cp.stdout.on('error', onerror);
+      // Cp.stdout.on('end', onend);
+      cp.stdout.resume();
+
+      function ondata(chunk) {
+        console.log('ondata', chunk);
+        cp.stdout.pause();
+        cleanup();
+        resolve(chunk);
+      }
+
+      function onend() {
+        cleanup();
+        resolve(null);
+      }
+
+      function onerror(err) {
+        cp.stdout.removeListener('error', onMissedError);
+        cleanup();
+        reject(err);
+      }
+
+      function cleanup() {
+        cp.stdout.removeListener('data', ondata);
+        cp.stdout.removeListener('error', onerror);
+        // Cp.stdout.removeListener('end', onend);
+      }
+    });
+  };
+
+}
 
 function stdoutStream(cp) {
+  let rs = Readable();
+  let reader = readStream(cp);
 
+  rs._read = function () {
+    console.log('reading');
+    reader()
+      .then(chunk => {
+        console.log(chunk);
+        this.push(chunk);
+      })
+      .catch(err => this.emit('error', err));
+  };
+
+  return rs;
 }
 
 function stdoutString(cp) {
@@ -14,15 +108,15 @@ function stdoutString(cp) {
     let stdout = [];
     let stderr = [];
 
-    child.stdout.on('data', (data) => {
+    cp.stdout.on('data', (data) => {
       stdout.push(data);
     });
 
-    child.stderr.on('data', (data) => {
+    cp.stderr.on('data', (data) => {
       stderr.push(data);
     });
 
-    child.on('exit', () => {
+    cp.on('exit', () => {
       if (stderr.length) {
         return reject(new Error(Buffer.concat(stderr).toString()));
       }
@@ -33,12 +127,13 @@ function stdoutString(cp) {
 }
 
 /**
-* @param {String|Buffer|Stream} file
+* @param {String} file
 * @param {String} outputFormat
 * @param {Object} options
 */
 unoconv.convert = function (file, outputFormat, options) {
   return new Promise(function (resolve, reject) {
+    options = options || {};
     let bin = 'unoconv';
 
     let args = ['-f' + outputFormat, '--stdout'];
@@ -47,9 +142,7 @@ unoconv.convert = function (file, outputFormat, options) {
       args.push('-p' + options.port);
     }
 
-    if (Buffer.isBuffer(file) || typeof file.pipe === 'function') {
-      args.push('--stdin');
-    } else if (typeof file === 'string') {
+    if (typeof file === 'string') {
       args.push(file);
     } else {
       return reject(new Error('wrong file'));
@@ -60,12 +153,6 @@ unoconv.convert = function (file, outputFormat, options) {
     }
 
     let child = childProcess.spawn(bin, args);
-
-    if (Buffer.isBuffer(file)) {
-      child.stdin.write(file);
-    } else if (typeof file.pipe === 'function') {
-      file.pipe(child.stdin);
-    }
 
     if (options.returnStream) {
       return resolve(stdoutStream(child));
