ef3071cc0276d29ab21541c2c2240827009fa878
Dan Krasilnikov <krasilneg@yandex.ru>
2018-02-16 21:47:41 +1000

    #IONCORE-372 фикс мерджа с develop

--- core/User.js
+++ core/User.js
@@ -16,6 +16,10 @@ function User(data, coactors, tz) {
     return data.name || this.id();
   };
 
+  this.type = function () {
+    return data.type;
+  };
+
   this.email = function () {
     return (data.properties && data.properties.email) || data.email;
   };
@@ -42,7 +46,7 @@ function User(data, coactors, tz) {
 
   this.setCoactors = function (ca2) {
     ca = Object.assign(ca, ca2);
-  }
+  };
 
   this.setProperties = function (properties) {
     data.properties = Object.assign(data.properties || {}, properties);
