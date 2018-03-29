7ca37b885f08c988248b07c579933b05c9679202
Андрей К <akumidv@yandex.ru>
2018-03-30 08:47:15 +1000

    IONCORE-412 проверка навигации, по наследованию, наличия классов

--- test/quality/meta-class-link.spec.js
+++ test/quality/meta-class-link.spec.js
@@ -19,62 +19,59 @@ describe('# Проверка достижимости классов из нав
 });
 
 function checkMetaLinks(pathApplications, pathApp) {
-  it(`Проверка достижимости классов из навигации в приложении ${pathApp}`, (done) => {
-    let meta = getMetaFiles(path.join(pathApplications, pathApp, 'meta'));
-    let navigation = getMetaFiles(path.join(pathApplications, pathApp, 'navigation'));
-
+  describe(`Проверка достижимости классов из навигации в приложении ${pathApp}`, () => {
+    let meta = {};
+    let navigation = {};
     let metaLink = {};
     let metaCheckLink = [];
-    Object.keys(navigation).forEach((navItem)=> {
-      if (navigation[navItem].type === 1) {
-        metaLink[navigation[navItem].classname] = true;
-        metaCheckLink.push(navigation[navItem].classname);
-      }
+    before('Инициализация меты', () => {
+      meta = getMetaFiles(path.join(pathApplications, pathApp, 'meta'));
+      navigation = getMetaFiles(path.join(pathApplications, pathApp, 'navigation'));
     });
-    let childNotLink = [];
-    Object.keys(meta).forEach((metaClassName)=> {  // TODO как-то не так работает, не должны  childNotLink быть в metaLink - а project добавляется
-      if (meta[metaClassName].ancestor) {
-        if (metaLink[meta[metaClassName].ancestor]) { // Родитель уже есть среди ссылочных объектов, значит достижим от навигации, добавляем
-          console.log('@@', meta[metaClassName].ancestor, meta[metaClassName].name);
-          metaLink[meta[metaClassName].name] = true;
-          metaCheckLink.push(meta[metaClassName].name);
-        } else {
-          childNotLink.push(meta[metaClassName].name);
+    it('Связываем классы по навигации и что такие классы есть в мете', () => {
+      let errMeta = [];
+      let startingMetaLink = Object.keys(metaLink).length;
+      Object.keys(navigation).forEach((navItem)=> { // Отбираем классы по навигации
+        if (meta[navigation[navItem].classname]) {
+          if (navigation[navItem].type === 1) {
+            metaLink[navigation[navItem].classname] = true;
+            metaCheckLink.push(navigation[navItem].classname);
+          }
+        } else if (navigation[navItem].type === 1) {
+          console.error(`В навигации ${navItem} ссылка на отсутствующий калсс ${navigation[navItem].classname}`);
+          errMeta.push(navigation[navItem].classname);
         }
+      });
+      if (errMeta.length) {
+        throw (new Error (`В файлах метаданных ссылки на некоректные классы ${errMeta}`));
       }
+      console.info('При анализе навигации добавлено ссылок на мету', Object.keys(metaLink).length - startingMetaLink);
     });
-    let childNotLink2 = [];
-    childNotLink.forEach((metaClassName)=> {  // TODO как-то не так работает, после второго прохода должны были уменьшится
-      if (meta[metaClassName].ancestor) {
-        if (metaLink[meta[metaClassName].ancestor]) { // Родитель уже есть среди ссылочных объектов, значит достижим от навигации, добавляем
-          console.log('@@', meta[metaClassName].ancestor, meta[metaClassName].name);
-          metaLink[meta[metaClassName].name] = true;
-          metaCheckLink.push(meta[metaClassName].name);
-        } else {
-          childNotLink2.push(meta[metaClassName].name);
-        }
+    it.skip('Связываем классы по ссылкам и коллекциям и что такие классы естьв  мете', () => {
+
+    });
+
+    it('Проверяем связанные классы по иерархии наследования и проверяем что такие классы есть в мете', () => {
+      let startingMetaLink = Object.keys(metaLink).length;
+      let childFailedLink = checkAncestor(Object.keys(meta),meta, metaLink, metaCheckLink);
+      if (Object.keys(childFailedLink.ancestor).length) {
+        console.warn('Классы проверенные от навигации, через ссылки и коллекции и иерархию наследования ' +
+          'по которым нет связей в мете',
+          Object.keys(childFailedLink.ancestor));
+        // Не является ошибкой, т.к. классы могут быть throw (new Error ('В файлах метаданных есть классы в иерархии, которые нигде не используются'));
       }
+      if (Object.keys(childFailedLink.errNames).length) {
+        throw (new Error
+          (`В иерархии наследованиия, некорректные родительские классы ${Object.keys(childFailedLink.errNames)}`));
+      }
+      console.info('При анализе наследования добавлено ссылок на мету', Object.keys(metaLink).length - startingMetaLink);
     });
-    console.log('childNotLink', childNotLink);
-    console.log('childNotLink2', childNotLink2);
-    // Проверка на родителя и доформирование metaCheckLink
-    // Проверяем все связи по свойствам
-    // metaCheckLink.forEach((className)=> {
+    it.skip('Проверка представлений, для которых нет классов', () => {
 
-    //});
-    console.log('#', Object.keys(metaLink));
+    });
+    it.skip('Проверка бизнес-процессов, для которых нет классов', () => {
 
-
-
-
-    done();
-
-/*    if (errFiles.length) {
-      done(new Error ('В файлах метаданных и данных ошибка в формате JSON'));
-    } else {
-      console.info('Проверенно JSON файлов', filesList.length);
-      done();
-    }*/
+    });
   });
 }
 
@@ -82,3 +79,40 @@ function getaAppList(appSourcePath) {
   return getaDirList(appSourcePath).dirList;
 }
 
+function checkAncestor(metaNames, meta, metaLink, metaCheckLink, childNotLinkLen = 0) {
+  let checked = {ancestor:{}, errNames:{}};
+  metaNames.forEach((className)=> {
+    if (meta[className]) { // Класс есть в мете
+      if (meta[className].ancestor) { // Есть родитель?
+        if (meta[meta[className].ancestor]) { // Родитель есть в классе меты
+          if (metaLink[meta[className].ancestor]) { // Родитель уже есть среди ссылочных объектов, значит достижим от навигации, добавляем наследника
+            metaLink[meta[className].name] = true;
+            metaCheckLink.push(meta[className].name);
+          } else if (metaLink[meta[className].name]) { // Наследник уже есть среди ссылочных объектов, значит достижим от навигации, добавляем родителя
+            metaLink[meta[className].ancestor] = true;
+            metaCheckLink.push(meta[className].ancestor);
+          } else {
+            checked.ancestor[meta[className].name] = true;
+            checked.ancestor[meta[className].ancestor] = true;
+          }
+        } else {
+          console.error(`В классе ${className} отсутствующий в мете родительский класс ${meta[className].ancestor}`);
+          checked.errNames[meta[className].ancestor] = true;
+        }
+      }
+    } else {
+      console.error(`В мете при проверке наследования, отсутствующий класс ${className}`);
+      checked.errNames[className] = true;
+    }
+  });
+  if (Object.keys(checked.ancestor).length !== 0 &&
+    Object.keys(checked.ancestor).length !== childNotLinkLen) {
+    let checkedNew = checkAncestor(Object.keys(checked.ancestor), meta, metaLink, metaCheckLink,
+      Object.keys(checked.ancestor).length);
+    checked.ancestor = checkedNew.ancestor;
+    Object.keys(checkedNew.errNames).forEach((errName)=> {
+      checked.errNames[errName] = checkedNew.errNames[errName];
+    })
+  }
+  return checked;
+}
