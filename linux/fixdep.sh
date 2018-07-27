f37bd672ceb2d032e5d225dd896cd677178de5d3
Сергей Владимиров <ivc.vladimirovsa@gmail.com>
2018-07-27 11:54:04 +1000

    IONEXAMPLE-165 $NODE_PATH Update fixdep.sh

--- linux/fixdep.sh
+++ linux/fixdep.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 SCRIPTPATH=$(dirname $0)
-ln -s "$SCRIPTPATH/libpng12.so.0.50.0" "$SCRIPTPATH/../node_modules/canvas-prebuilt/canvas/build/Release/libpng12.so.0"
+ln -s "$NODE_PATH/linux/libpng12.so.0.50.0" "$SCRIPTPATH/../node_modules/canvas-prebuilt/canvas/build/Release/libpng12.so.0"
