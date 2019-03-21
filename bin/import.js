79301e6e848e34fc8aabf08cfd2138f5926493a7
Dan Krasilnikov <krasilneg@yandex.ru>
2019-03-21 13:17:55 +1000

    #IONCORE-589 Импорт данных полностью выпилен из скрипта импорта

--- bin/import.js
+++ bin/import.js
@@ -4,14 +4,12 @@
  * Created by kras on 10.07.16.
  */
 const metaImporter = require('lib/import-meta');
-const dataImporter = require('lib/import-data');
 const config = require('../config');
 const di = require('core/di');
 const IonLogger = require('core/impl/log/IonLogger');
 const errorSetup = require('core/error-setup');
 const alias = require('core/scope-alias');
 const extend = require('extend');
-const path = require('path');
 errorSetup(config.lang || 'ru');
 
 var sysLog = new IonLogger(config.log || {});
@@ -19,7 +17,6 @@ var sysLog = new IonLogger(config.log || {});
 var params = {
   src: '../in',
   ns: null,
-  importData: false,
   ignoreIntegrityCheck: true
 };
 
@@ -29,8 +26,6 @@ process.argv.forEach(function (val) {
   if (val === '--ignoreIntegrityCheck') {
     console.warn('При импорте игнорируется целостность данных, возможны ошибки в БД');
     params.ignoreIntegrityCheck = true;
-  } else if (val === '--with-data') {
-    params.importData = true;
   } else if (val.substr(0, 2) === '--') {
     setParam = val.substr(2);
   } else if (setParam) {
@@ -63,18 +58,6 @@ di('boot', config.bootstrap,
         metaRepo: scope.metaRepo,
         log: sysLog,
         namespace: params.ns
-      }).then(() => scope)
-  )
-  .then(scope => (!params.importData) ? scope :
-    dataImporter(path.join(params.src, 'data'),
-      {
-        metaRepo: scope.metaRepo,
-        dataRepo: scope.dataRepo,
-        workflows: scope.workflows,
-        sequences: scope.sequenceProvider,
-        log: sysLog,
-        namespace: params.ns,
-        ignoreIntegrityCheck: params.ignoreIntegrityCheck
       }).then(() => scope)
   )
   .then(scope => scope.dataSources.disconnect())
