c8413fa6494a7f4e9caa8a41ecf3b24b99294da0
Данил Колесник <kalias_90@live.com>
2019-04-11 18:50:34 +1000

    IONCORE-602

--- core/interfaces/DataRepository/lib/Item.js
+++ core/interfaces/DataRepository/lib/Item.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-lines */
+/* eslint-disable max-statements */
+/* eslint-disable func-names */
+
 /**
  * Created by Vasiliy Ermilov (email: inkz@xakep.ru, telegram: @inkz1) on 29.04.16.
  */
@@ -7,8 +11,6 @@ const Property = require('./Property');
 const PropertyTypes = require('core/PropertyTypes');
 const cast = require('core/cast');
 
-// jshint maxstatements: 30
-
 /**
  * @param {String} id
  * @param {{}} base
@@ -16,8 +18,7 @@ const cast = require('core/cast');
  * @constructor
  */
 function Item(id, base, classMeta) {
-
-  var _this = this;
+  const _this = this;
 
   /**
    * @type {String}
@@ -48,7 +49,7 @@ function Item(id, base, classMeta) {
 
   this.slCacheClean = true;
 
-  this.emptify = function () {
+  this.emptify = function() {
     this.id = null;
 
     this.base = {};
@@ -64,32 +65,32 @@ function Item(id, base, classMeta) {
     this.slCacheClean = true;
   };
 
-  this.getItemId = function () {
+  this.getItemId = function() {
     return this.id;
   };
 
-  this.getClassName = function () {
+  this.getClassName = function() {
     return this.classMeta.getCanonicalName();
   };
 
   /**
    * @returns {ClassMeta}
    */
-  this.getMetaClass = function () {
+  this.getMetaClass = function() {
     return this.classMeta;
   };
 
   /**
    * @returns {String}
    */
-  this.getCreator = function () {
+  this.getCreator = function() {
     return this.base._creator;
   };
 
   /**
    * @returns {String}
    */
-  this.getEditor = function () {
+  this.getEditor = function() {
     return this.base._editor;
   };
 
@@ -97,12 +98,10 @@ function Item(id, base, classMeta) {
    * @param {String} name
    * @returns {Item | null}
    */
-  this.getAggregate = function (name) {
-    let props = this.getProperties();
-    let p = props[name];
-    if (p && p.getType() === PropertyTypes.REFERENCE) {
+  this.getAggregate = function(name) {
+    const prop = this.getProperties()[name];
+    if (prop && prop.getType() === PropertyTypes.REFERENCE)
       return this.references[name] || null;
-    }
     return null;
   };
 
@@ -110,12 +109,10 @@ function Item(id, base, classMeta) {
    * @param {String} name
    * @returns {Array | null}
    */
-  this.getAggregates = function (name) {
-    let props = this.getProperties();
-    let p = props[name];
-    if (p && p.getType() === PropertyTypes.COLLECTION && this.collections) {
+  this.getAggregates = function(name) {
+    const prop = this.getProperties()[name];
+    if (prop && prop.getType() === PropertyTypes.COLLECTION && this.collections)
       return this.collections[name] || null;
-    }
     return null;
   };
 
@@ -165,32 +162,28 @@ function Item(id, base, classMeta) {
     }
   }
 
-  this.get = function (name) {
-    if (name === '__class') {
+  this.get = function(name) {
+    if (name === '__class')
       return this.getClassName();
-    }
 
-    if (name === '__classTitle') {
+    if (name === '__classTitle')
       return this.getMetaClass().getCaption();
-    }
 
-    var dot = name.indexOf('.');
+    const dot = name.indexOf('.');
     if (dot > -1) {
-      let pn = name.substring(0, dot);
-      let props = this.getProperties();
+      const pn = name.substring(0, dot);
+      const props = this.getProperties();
       if (props.hasOwnProperty(pn)) {
         if (props[pn].getType() === PropertyTypes.REFERENCE) {
-          let i = this.getAggregate(pn);
-          if (i) {
+          const i = this.getAggregate(pn);
+          if (i)
             return i.get(name.substring(dot + 1));
-          }
         } else if (props[pn].getType() === PropertyTypes.COLLECTION) {
-          let is = this.getAggregates(pn);
+          const is = this.getAggregates(pn);
           if (Array.isArray(is)) {
-            let result = [];
-            for (let i = 0; i < is.length; i++) {
+            const result = [];
+            for (let i = 0; i < is.length; i++)
               result.push(is[i].get(name.substring(dot + 1)));
-            }
             return result;
           }
         }
@@ -200,13 +193,12 @@ function Item(id, base, classMeta) {
     return getFromBase(name);
   };
 
-  this.set = function (name, value) {
-    let dot = name.indexOf('.');
+  this.set = function(name, value) {
+    const dot = name.indexOf('.');
     if (dot > -1) {
-      let i = this.getAggregate(name.substring(0, dot));
-      if (i) {
+      const i = this.getAggregate(name.substring(0, dot));
+      if (i)
         i.set(name.substring(dot + 1), value);
-      }
     } else {
       setToBase(name, value);
     }
@@ -219,15 +211,12 @@ function Item(id, base, classMeta) {
   function findPropertyMeta(nm, cm) {
     let dot;
     if ((dot = nm.indexOf('.')) > -1) {
-      let pm = cm.getPropertyMeta(nm.substring(0, dot));
-      if (!pm) {
-        throw new Error('Не найден атрибут ' + nm + ' класса ' + cm.getCanonicalName());
-      }
-      if (pm.type === PropertyTypes.REFERENCE || pm.type === PropertyTypes.COLLECTION) {
+      const pm = cm.getPropertyMeta(nm.substring(0, dot));
+      if (!pm)
+        throw new Error(`Не найден атрибут ${nm} класса ${cm.getCanonicalName()}`);
+      if (pm.type === PropertyTypes.REFERENCE || pm.type === PropertyTypes.COLLECTION)
         return findPropertyMeta(nm.substring(dot + 1), pm._refClass);
-      } else {
-        return null;
-      }
+      return null;
     }
     return cm.getPropertyMeta(nm);
   }
@@ -236,16 +225,13 @@ function Item(id, base, classMeta) {
    * @param {String} name
    * @returns {Property | null}
    */
-  this.property = function (name) {
-    let props = this.getProperties();
-    if (props.hasOwnProperty(name)) {
+  this.property = function(name) {
+    const props = this.getProperties();
+    if (props.hasOwnProperty(name))
       return props[name];
-    } else {
-      let pm = findPropertyMeta(name, this.classMeta);
-      if (pm) {
-        return new Property(this, pm, name);
-      }
-    }
+    const pm = findPropertyMeta(name, this.classMeta);
+    if (pm)
+      return new Property(this, pm, name);
     return null;
   };
 
@@ -275,76 +261,80 @@ function Item(id, base, classMeta) {
   /**
    * @param {{}} [needed]
    * @param {Boolean} [cached]
+   * @param {Boolean} [recursive]
    * @returns {Promise}
    */
-  this.calculateProperties = function (needed, cached, recursive) {
+  this.calculateProperties = function(needed, cached, recursive) {
     let calculations = Promise.resolve();
     if (!this.calculating) {
       this.calculating = true;
-      let props = this.getMetaClass().getPropertyMetas();
-      recursive = (recursive === false) ? false : true;
+      const props = this.getMetaClass().getPropertyMetas();
+      recursive = recursive !== false;
 
       this.stringValue = this.stringValue || id;
-      if (this.classMeta.isSemanticCached()) {
+      if (this.classMeta.isSemanticCached())
         this.stringValue = this.base.__semantic;
-      }
 
       if (!this.classMeta.isSemanticCached()) {
         calculations = calculations
           .then(() => this.classMeta.getSemantics(this))
-          .then((v) => {
-            this.stringValue = v;
+          .then((val) => {
+            this.stringValue = val;
           });
       }
 
-      props.forEach((p) => {
-        if (!needed || needed.hasOwnProperty(p.name)) {
-          if (p.type === PropertyTypes.COLLECTION) {
+      props.forEach((prop) => {
+        if (!needed || needed.hasOwnProperty(prop.name)) {
+          if (prop.type === PropertyTypes.COLLECTION) {
             let result = '';
-            let agregates = this.property(p.name).evaluate();
+            const agregates = this.property(prop.name).evaluate();
             if (Array.isArray(agregates)) {
               agregates.forEach((item) => {
                 calculations = calculations
+                  // eslint-disable-next-line no-confusing-arrow
                   .then(() => recursive ? item.calculateProperties(null, cached, recursive) : null)
-                  .then(() => (typeof p.semanticGetter === 'function') ? p.semanticGetter.apply(item) : item.toString())
-                  .then((v) => {
-                    result = result + (result ? ' ' : '') + v;
+                  // eslint-disable-next-line no-confusing-arrow
+                  .then(() => (typeof prop.semanticGetter === 'function') ? prop.semanticGetter.apply(item) : item.toString())
+                  .then((val) => {
+                    result = result + (result ? ' ' : '') + val;
                   });
               });
               calculations = calculations.then(() => {
-                this.properties[p.name].displayValue = result;
+                this.properties[prop.name].displayValue = result;
               });
             }
           }
 
-          if (p.type === PropertyTypes.REFERENCE) {
-            let agr = this.property(p.name).evaluate();
+          if (prop.type === PropertyTypes.REFERENCE) {
+            const agr = this.property(prop.name).evaluate();
             if (agr) {
               calculations = calculations
+                // eslint-disable-next-line no-confusing-arrow
                 .then(() => recursive ? agr.calculateProperties(null, cached, recursive) : null)
-                .then(() => (typeof p.semanticGetter === 'function') ? p.semanticGetter.apply(agr) : agr.toString())
-                .then((v) => {
-                  this.properties[p.name].displayValue = v;
+                // eslint-disable-next-line no-confusing-arrow
+                .then(() => (typeof prop.semanticGetter === 'function') ? prop.semanticGetter.apply(agr) : agr.toString())
+                .then((val) => {
+                  this.properties[prop.name].displayValue = val;
                 });
             }
           }
         }
       });
 
-      props.forEach((p) => {
-        if (p._formula && (!p.cached || cached) && (!needed || needed.hasOwnProperty(p.name))) {
-          calculations = calculations.then(() => p._formula.apply(this))
+      props.forEach((prop) => {
+        if (prop._formula && (!prop.cached || cached) && (!needed || needed.hasOwnProperty(prop.name))) {
+          calculations = calculations.then(() => prop._formula.apply(this))
             .then((result) => {
-              if (p.type === PropertyTypes.REFERENCE && result instanceof Item) {
-                this.references[p.name] = result;
-                this.calculated[p.name] = result.getItemId();
-              } else if (p.type === PropertyTypes.COLLECTION
-                && Array.isArray(result)
-                && result.length === result.filter(r => r instanceof Item).length) {
-                this.collections[p.name] = result;
-                this.calculated[p.name] = result.map(r => r.getItemId());
+              if (prop.type === PropertyTypes.REFERENCE && result instanceof Item) {
+                this.references[prop.name] = result;
+                this.calculated[prop.name] = result.getItemId();
+              } else if (prop.type === PropertyTypes.COLLECTION &&
+                Array.isArray(result) &&
+                result.length === result.filter(res => res instanceof Item).length) {
+                this.collections[prop.name] = result;
+                this.calculated[prop.name] = result.map(res => res.getItemId());
               } else {
-                this.calculated[p.name] = cast(result, p.type);
+                this.calculated[prop.name] = cast(result, prop.type);
               }
             });
         }
@@ -353,19 +343,19 @@ function Item(id, base, classMeta) {
       if (!this.classMeta.isSemanticCached()) {
         calculations = calculations
           .then(() => this.classMeta.getSemantics(this))
-          .then((v) => {
-            this.stringValue = v;
+          .then((val) => {
+            this.stringValue = val;
           });
       }
+      calculations.then(() => {
+        delete this.calculating;
+      });
     }
-    return calculations.then(() => {
-      delete this.calculating;
-      return this;
-    });
+    return calculations.then(() => this);
   };
 }
 
-Item.prototype.toString = function () {
+Item.prototype.toString = function() {
   return this.stringValue || this.getItemId();
 };
 
