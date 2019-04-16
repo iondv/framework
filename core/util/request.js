798ca83d3f4ccd42aee12fbd56fd02dbf8339e37
Dan Krasilnikov <krasilneg@yandex.ru>
2019-04-16 17:57:05 +1000

    #IONCORE-593 код-ревью

--- core/util/request.js
+++ core/util/request.js
@@ -4,13 +4,8 @@ const request = require('request');
  * @param {{}} reqParams
  * @returns {Promise.<Object>}
  */
-module.exports = function requester(reqParams) {
-  return new Promise((resolve, reject) => {
-    request(reqParams, (err, res, body) => {
-      if (err) {
-        return reject(err);
-      }
-      return resolve(body);
-    });
-  });
-};
+module.exports = reqParams => new Promise(
+  (resolve, reject) => {
+    request(reqParams, (err, res, body) => err ? reject(err) : resolve(body));
+  }
+);
