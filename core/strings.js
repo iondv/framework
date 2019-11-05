9132146bceeb18fef67795702c20453ece21903b
Vasilii Ermilov <ermilov.work@yandex.ru>
2019-11-05 18:13:46 +1000

    Доработки модуля strings
    IONCORE-633

--- core/strings.js
+++ core/strings.js
@@ -5,22 +5,7 @@
 const merge = require('merge');
 const strBase = {};
 
-/**
- * @param {String} prefix
- * @param {{}} base
- */
-module.exports.registerBase = function (prefix, base) {
-  if (prefix && base) {
-    strBase[prefix] = merge(base, strBase[prefix] || {});
-  }
-};
-
-/**
- * @param {String} prefix
- * @param {String} id
- * @param {{}} params
- */
-module.exports.s = function (prefix, ...ids) {
+function strings(prefix, ...ids) {
   const params = (ids.length && typeof ids[ids.length - 1] !== 'string') ? ids.pop() : null;
   if (prefix && ids.length) {
     if (strBase.hasOwnProperty(prefix)) {
@@ -39,4 +24,23 @@ module.exports.s = function (prefix, ...ids) {
     return ids[ids.length - 1];
   }
   return '';
+}
+
+/**
+ * @param {String} prefix
+ * @param {{}} base
+ */
+module.exports.registerBase = function (prefix, base) {
+  if (prefix && base) {
+    strBase[prefix] = merge(base, strBase[prefix] || {});
+  }
 };
+
+/**
+ * @param {String} prefix
+ * @param {String} id
+ * @param {{}} params
+ */
+module.exports.s = strings;
+
+module.exports.unprefix = prefix => (str, params) => strings(prefix, str, params);
