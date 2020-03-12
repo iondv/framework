4ffd09f4826c6b960bdb7ca4dc53ca461dfa149d
Данил Колесник <kolesnik@rollbyte.com>
2020-03-12 18:39:35 +1000

    IONCORE-649

--- core/impl/datasource/postgresql.js
+++ core/impl/datasource/postgresql.js
@@ -1,77 +1,111 @@
 const DataSource = require('core/interfaces/DataSource');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const LoggerProxy = require('core/impl/log/LoggerProxy');
+const clone = require('fast-clone');
+const fs = require('fs');
 
+/**
+ * @param {*} config
+ * @param {*} config.logger
+ * @param {*} config.options
+    user?: string;
+    database?: string;
+    password?: string;
+    port?: number;
+    host?: string;
+    connectionString?: string;
+    keepAlive?: boolean;
+    stream?: stream.Duplex;
+    statement_timeout?: false | number;
+    parseInputDatesAsUTC?: boolean;
+    ssl?: boolean | ConnectionOptions;
+    query_timeout?: number;
+    keepAliveInitialDelayMillis?: number;
+    max?: number;
+    min?: number;
+    connectionTimeoutMillis?: number;
+    idleTimeoutMillis?: number;
+    log?: (...messages: any[]) => void;
+    application_name?: string;
+    Promise?: PromiseConstructorLike;
+ */
 function PostgreSQL(config) {
-
   const _this = this;
   const log = config.logger || new LoggerProxy();
 
-  this.client = null;
-  this.isOpen = false;
-  this.busy = false;
+  this.pool = null;
+
+  /**
+   * @param {String[]|String} opts
+   */
+  const readFiles = (opts) => {
+    if (Array.isArray(opts)) {
+      let result = Promise.resolve();
+      const vs = [];
+      opts.forEach((val) => {
+        result = result
+          .then(() => readFiles(val))
+          .then(data => vs.push(data));
+      });
+      return result.then(() => vs);
+    }
+    return new Promise((res, rej) => fs.readFile(opts, (err, data) => {
+      if (err)
+        return rej(err);
+      return res(data);
+    }));
+  };
 
   /**
    * @returns {*}
    */
-  this._connection = function () {
-    if (_this.client && _this.isOpen) {
-      return _this.client;
-    }
-    return null;
+  this._connection = () => {
+    if (_this.pool)
+      return _this.pool.connect();
+    return Promise.resolve();
   };
 
   /**
    * @returns {Promise}
    */
-  this._open = function () {
-
-    if (!_this.client) {
-      _this.client = new Client({
-        user?: string, // default process.env.PGUSER || process.env.USER
-        password?: string, //default process.env.PGPASSWORD
-        database?: string, // default process.env.PGDATABASE || process.env.USER
-        port?: number, // default process.env.PGPORT
-        connectionString?: string, // e.g. postgres://user:password@host:5432/database
-        ssl?: any, // passed directly to node.TLSSocket
-        types?: any, // custom type parsers
-        statement_timeout?: number, // number of milliseconds before a statement in query will time out, default is no timeout
-        query_timeout?: number, // number of milliseconds before a query call will timeout, default is no timeout
-      });
-      _this.client.on('end', () => {
-        this.isOpen = false;
-        this.busy = false;
+  this._open = () => {
+    let result = Promise.resolve();
+    if (!_this.pool) {
+      const opts = clone(config.options);
+      if (opts.ssl) {
+        if (opts.ssl.ca) {
+          result = result
+            .then(() => readFiles(opts.ssl.ca))
+            .then((bufs) => {opts.ssl.ca = bufs;});
+        }
+        if (opts.ssl.key) {
+          result = result
+            .then(() => readFiles(opts.ssl.key))
+            .then((bufs) => {opts.ssl.key = bufs;});
+        }
+        if (opts.ssl.cert) {
+          result = result
+            .then(() => readFiles(opts.ssl.cert))
+            .then((bufs) => {opts.ssl.cert = bufs;});
+        }
+      }
+      return result.then(() => {
+        _this.pool = new Pool(opts);
+        _this.pool.on('connect', client => log.info('connect'));
+        _this.pool.on('acquire', client => log.info('acquire'));
+        _this.pool.on('error', (err, client) => log.error(err));
+        _this.pool.on('remove', client => log.info('remove'));
       });
     }
-
-    if (_this.isOpen && !_this.busy) {
-      return Promise.resolve(true);
-    } else if (!_this.isOpen && _this.busy) {
-      return new Promise((resolve) => {
-        _this.client.once('isOpen', () => {
-          resolve(true);
-        });
-      });
-    } else {
-      _this.busy = true;
-      return _this.client.connect()
-        .then(() => {
-          _this.isOpen = true;
-          _this.busy = false;
-          _this.client.emit('isOpen', _this.client);
-          //TODO get baseName??
-          log.info('Получено соединение с базой: ' + baseName);
-          return true;
-        });
-    }
+    return Promise.resolve();
   };
 
   /**
    * @returns {Promise}
    */
-  this._close = function () {
-    if (_this.client && _this.isOpen) {
-      return _this.client.end();
+  this._close = () => {
+    if (_this.pool) {
+      return _this.pool.end();
     }
     return Promise.resolve();
   };
